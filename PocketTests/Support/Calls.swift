92a65e8c592020cca385807f14c40778b48b8d09
Jacob Morris <jmorris@mozilla.com>
2021-05-11 15:36:47 -0700

    feat(auth): persist access token
    Co-authored-by: David Skuza <dskuza@getpocket.com>

---
+++ PocketTests/Support/Calls.swift
@@ -0,0 +1,20 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+struct Calls<T> {
+    private var calls: [T] = []
+
+    mutating func add(_ call: T) {
+        calls.append(call)
+    }
+
+    var wasCalled: Bool {
+        !calls.isEmpty
+    }
+
+    var last: T? {
+        return calls.last
+    }
+}
