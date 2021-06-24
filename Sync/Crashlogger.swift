e9c80d3204318c02d004b2583301409a7524b5b7
Jacob Morris <jmorris@mozilla.com>
2021-06-24 11:11:21 -0700

    Pull Sentry down into Sync
    This is necessary because we'll need to do some Sentry-related work
    inside of Sync, but because of the nature of Swift Package Manager, we
    can only have one target that depends on a package. Thus, we have Sync
    depend on the Sentry SDK and create a wrapper (Crashlogger) that can be
    used within the app target.
    
    This is a workaround until we resolve the dependency strategy issues
    more broadly.

---
+++ Sync/Crashlogger.swift
@@ -0,0 +1,23 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import Sentry
+
+
+public struct Crashlogger {
+    public static func setUserID(_ userID: String) {
+        SentrySDK.setUser(User(userId: userID))
+    }
+
+    public static func capture(error: Error) {
+        SentrySDK.capture(error: error)
+    }
+
+    public static func start(dsn: String) {
+        SentrySDK.start { options in
+            options.dsn = dsn
+            options.debug = true
+        }
+    }
+}
