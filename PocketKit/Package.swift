50f7fdc471af7266280557188793c1601758953a
David Skuza <dskuza@getpocket.com>
2021-09-23 15:23:57 -0500

    Add missing license headers

--- PocketKit/Package.swift
+++ PocketKit/Package.swift
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 // swift-tools-version:5.4
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
