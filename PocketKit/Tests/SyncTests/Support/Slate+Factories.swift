495f02cdb366f81c8bf72997ca6884def56703b1
David Skuza <dskuza@getpocket.com>
2021-10-08 15:41:41 -0500

    Sync: Support saving a recommendation via Source
    https://getpocket.atlassian.net/browse/IN-191
    
    Co-authored-by: David Skuza<dskuza@getpocket.com>

--- PocketKit/Tests/SyncTests/Support/Slate+Factories.swift
+++ PocketKit/Tests/SyncTests/Support/Slate+Factories.swift
@@ -4,12 +4,16 @@
 extension Slate {
     static func build(
         id: String = "slate-1",
+        requestID: String = "slate-1-request",
+        experimentID: String = "slate-1-experiment",
         name: String = "A slate",
         description: String = "For use in tests",
         recommendations: [Slate.Recommendation] = []
     ) -> Slate {
         Slate(
             id: id,
+            requestID: requestID,
+            experimentID: experimentID,
             name: name,
             description: description,
             recommendations: recommendations
