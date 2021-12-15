34ce978bc8ac07cf97f4966af978936c5126ac60
Jacob Morris <jmorris@mozilla.com>
2021-12-15 10:45:00 -0800

    Add/remove icons for MyList cells
    - Add "favorite" icon (a star)
    - Add "overflow" icon (a horizontal ellipses)
    - Add "share" icon
    - Remove old horizontal overflow (replaced)
    - Remove vertical overflow (unused)

--- PocketKit/Sources/Textile/Style/Icons/ImageAsset.swift
+++ PocketKit/Sources/Textile/Style/Icons/ImageAsset.swift
@@ -9,9 +9,11 @@ public struct ImageAsset {
 extension ImageAsset {
     public static let save = ImageAsset("save")
     public static let saved = ImageAsset("saved")
-    public static let horizontalOverflow = ImageAsset("horizontalOverflow")
-    public static let verticalOverflow = ImageAsset("verticalOverflow")
     public static let alert = ImageAsset("alert")
+    public static let overflow = ImageAsset("overflow")
+    public static let favorite = ImageAsset("favorite")
+    public static let favoriteFilled = ImageAsset("favorite.filled")
+    public static let share = ImageAsset("share")
     public static let radioSelected = ImageAsset("radioSelected")
     public static let radioDeselected = ImageAsset("radioDeselected")
 }
