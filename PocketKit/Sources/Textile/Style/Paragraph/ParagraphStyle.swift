8c2fa41f0033dad99b933bffe6d6022127c5937e
Jacob Morris <jmorris@mozilla.com>
2021-12-20 13:42:31 -0800

    Textiles: add support for line spacing

--- PocketKit/Sources/Textile/Style/Paragraph/ParagraphStyle.swift
+++ PocketKit/Sources/Textile/Style/Paragraph/ParagraphStyle.swift
@@ -10,26 +10,33 @@ public struct ParagraphStyle {
     public let alignment: TextAlignment
     public let lineBreakMode: LineBreakMode
     public let lineSpacing: CGFloat?
+    public let lineHeight: CGFloat?
 
     public init(
         alignment: TextAlignment,
         lineBreakMode: LineBreakMode = .none,
-        lineSpacing: CGFloat? = nil
+        lineSpacing: CGFloat? = nil,
+        lineHeight: CGFloat? = nil
     ) {
         self.alignment = alignment
         self.lineBreakMode = lineBreakMode
         self.lineSpacing = lineSpacing
+        self.lineHeight = lineHeight
     }
 
     public func with(alignment: TextAlignment) -> ParagraphStyle {
-        return ParagraphStyle(alignment: alignment, lineBreakMode: lineBreakMode, lineSpacing: lineSpacing)
+        return ParagraphStyle(alignment: alignment, lineBreakMode: lineBreakMode, lineSpacing: lineSpacing, lineHeight: lineHeight)
     }
 
     public func with(lineBreakMode: LineBreakMode) -> ParagraphStyle {
-        return ParagraphStyle(alignment: alignment, lineBreakMode: lineBreakMode, lineSpacing: lineSpacing)
+        return ParagraphStyle(alignment: alignment, lineBreakMode: lineBreakMode, lineSpacing: lineSpacing, lineHeight: lineHeight)
     }
 
     public func with(lineSpacing: CGFloat?) -> ParagraphStyle {
-        return ParagraphStyle(alignment: alignment, lineBreakMode: lineBreakMode, lineSpacing: lineSpacing)
+        return ParagraphStyle(alignment: alignment, lineBreakMode: lineBreakMode, lineSpacing: lineSpacing, lineHeight: lineHeight)
+    }
+
+    public func with(lineHeight: CGFloat?) -> ParagraphStyle {
+        return ParagraphStyle(alignment: alignment, lineBreakMode: lineBreakMode, lineSpacing: lineSpacing, lineHeight: lineHeight)
     }
 }
