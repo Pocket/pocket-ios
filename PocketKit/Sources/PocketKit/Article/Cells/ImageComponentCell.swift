3f8407fd2b9657d495ccdd54e1f772d8445164bf
Jacob Morris <jmorris@mozilla.com>
2021-12-21 16:10:26 -0800

    Reader: Update image component layout
    Caption and Credit are now combined into a single text view (using
    different attributes for each).
    
    Images have a bottom margin of 24pt

--- PocketKit/Sources/PocketKit/Article/Cells/ImageComponentCell.swift
+++ PocketKit/Sources/PocketKit/Article/Cells/ImageComponentCell.swift
@@ -3,53 +3,37 @@ import Kingfisher
 
 
 class ImageComponentCell: UICollectionViewCell {
+    enum Constants {
+        static let captionSpacing: CGFloat = 8
+        static let layoutMargins = UIEdgeInsets(top: 0, left: 0, bottom: 24, right: 0)
+    }
+
+    struct ImageSpec {
+        let source: URL
+        let size: CGSize
+    }
+
+    struct Model {
+        let caption: NSAttributedString?
+        let image: ImageSpec?
+    }
+
     let imageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .center
         imageView.layer.cornerRadius = 4
         imageView.clipsToBounds = true
+        imageView.backgroundColor = UIColor(white: 0.95, alpha: 1)
         imageView.setContentCompressionResistancePriority(.required, for: .vertical)
         return imageView
     }()
 
-    var attributedCaption: NSAttributedString? {
-        get {
-            captionLabel.attributedText
-        }
-        set {
-            captionLabel.attributedText = newValue
-        }
-    }
-
-    var attributedCredit: NSAttributedString? {
-        get {
-            creditLabel.attributedText
-        }
-        set {
-            creditLabel.attributedText = newValue
-        }
-    }
-
-    private let captionLabel: UILabel = {
-        let label = UILabel()
-        label.numberOfLines = 0
-        label.isUserInteractionEnabled = true
-
-        return label
-    }()
-
-    private let creditLabel: UILabel = {
-        let label = UILabel()
-        label.numberOfLines = 0
-        label.isUserInteractionEnabled = true
-
-        return label
-    }()
+    private let captionTextView: UITextView = ArticleComponentTextView()
 
     private let stack: UIStackView = {
         let stack = UIStackView()
         stack.axis = .vertical
-        stack.spacing = 4
+        stack.spacing = Constants.captionSpacing
 
         return stack
     }()
@@ -59,19 +43,82 @@ class ImageComponentCell: UICollectionViewCell {
 
         contentView.addSubview(stack)
         stack.addArrangedSubview(imageView)
-        stack.addArrangedSubview(captionLabel)
-        stack.addArrangedSubview(creditLabel)
+        stack.addArrangedSubview(captionTextView)
 
+        contentView.layoutMargins = Constants.layoutMargins
         stack.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            stack.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
-            stack.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
-            stack.topAnchor.constraint(equalTo: contentView.topAnchor),
-            stack.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            stack.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor),
+            stack.trailingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.trailingAnchor),
+            stack.topAnchor.constraint(equalTo: contentView.layoutMarginsGuide.topAnchor),
+            stack.bottomAnchor.constraint(equalTo: contentView.layoutMarginsGuide.bottomAnchor),
         ])
     }
     
     required init?(coder: NSCoder) {
         fatalError("Unable to instantiate \(Self.self) from xib/storyboard")
+    }
+}
+
+extension ImageComponentCell {
+    func configure(model: Model, imageLoaded: ((UIImage) -> Void)? = nil) {
+        captionTextView.attributedText = model.caption
+        captionTextView.isHidden = model.caption == nil
+
+        imageView.image = nil
+        guard let imageSpec = model.image else {
+            return
+        }
+
+        imageView.kf.indicatorType = .activity
+        imageView.kf.setImage(
+            with: imageSpec.source,
+            options: [
+                .scaleFactor(UIScreen.main.scale),
+                .processor(
+                    OnlyResizeDownProcessor(size: imageSpec.size, mode: .aspectFit)
+                )
+            ]
+        ) { result in
+            switch result {
+            case .success(let result):
+                imageLoaded?(result.image)
+            case .failure:
+                break
+            }
+        }
+    }
+}
+
+private class OnlyResizeDownProcessor: ImageProcessor {
+    let identifier = "com.getpocket.image-processor.only-resize-down"
+
+    let resizingProcessor: ResizingImageProcessor
+
+    init(resizingProcessor: ResizingImageProcessor) {
+        self.resizingProcessor = resizingProcessor
+    }
+
+    convenience init(size: CGSize, mode: ContentMode) {
+        self.init(
+            resizingProcessor: ResizingImageProcessor(
+                referenceSize: size,
+                mode: mode
+            )
+        )
+    }
+
+    func process(item: ImageProcessItem, options: KingfisherParsedOptionsInfo) -> KFCrossPlatformImage? {
+        switch item {
+        case .image(let image):
+            guard image.size.height > resizingProcessor.referenceSize.height
+                    || image.size.width > resizingProcessor.referenceSize.width else {
+                        return image
+                    }
+
+            return resizingProcessor.process(item: item, options: options)
+        case .data:
+            return (DefaultImageProcessor.default |> self).process(item: item, options: options)
+        }
     }
 }
