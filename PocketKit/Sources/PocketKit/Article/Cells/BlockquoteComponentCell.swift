d1baca827cca15adeec7b558e3b75b428b1c8038
Jacob Morris <jmorris@mozilla.com>
2021-12-21 16:54:14 -0800

    Reader: Update blockquotes to reflect new designs

--- PocketKit/Sources/PocketKit/Article/Cells/BlockquoteComponentCell.swift
+++ PocketKit/Sources/PocketKit/Article/Cells/BlockquoteComponentCell.swift
@@ -4,12 +4,14 @@ import UIKit
 class BlockquoteComponentCell: UICollectionViewCell, ArticleComponentTextCell, ArticleComponentTextViewDelegate {
     
     struct Constants {
-        static let dividerWidth: CGFloat = 6
-        static let stackSpacing: CGFloat = 12
+        static let dividerHeight: CGFloat = 5
+        static let stackSpacing: CGFloat = 20
+        static let layoutMargins = UIEdgeInsets(top: 0, left: 0, bottom: 23, right: 0)
     }
+
     private lazy var divider: UIView = {
         let view = UIView()
-        view.backgroundColor = UIColor(.ui.grey6)
+        view.backgroundColor = UIColor(.ui.grey1)
         return view
     }()
     
@@ -20,9 +22,10 @@ class BlockquoteComponentCell: UICollectionViewCell, ArticleComponentTextCell, A
     }()
     
     private lazy var stackView: UIStackView = {
-       let stackView = UIStackView(arrangedSubviews: [divider, textView])
+        let stackView = UIStackView()
+        stackView.axis = .vertical
         stackView.spacing = Constants.stackSpacing
-        stackView.alignment = .center
+        stackView.alignment = .leading
         return stackView
     }()
     
@@ -41,17 +44,19 @@ class BlockquoteComponentCell: UICollectionViewCell, ArticleComponentTextCell, A
         super.init(frame: frame)
         
         contentView.addSubview(stackView)
+        stackView.addArrangedSubview(textView)
+        stackView.addArrangedSubview(divider)
 
-        divider.translatesAutoresizingMaskIntoConstraints = false
+        contentView.layoutMargins = Constants.layoutMargins
         stackView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            divider.widthAnchor.constraint(equalToConstant: Constants.dividerWidth),
-            divider.heightAnchor.constraint(equalTo: stackView.heightAnchor),
+            divider.widthAnchor.constraint(equalToConstant: 50),
+            divider.heightAnchor.constraint(equalToConstant: Constants.dividerHeight),
             
-            stackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
-            stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
-            stackView.topAnchor.constraint(equalTo: contentView.topAnchor),
-            stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            stackView.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.trailingAnchor),
+            stackView.topAnchor.constraint(equalTo: contentView.layoutMarginsGuide.topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: contentView.layoutMarginsGuide.bottomAnchor),
         ])
     }
     
