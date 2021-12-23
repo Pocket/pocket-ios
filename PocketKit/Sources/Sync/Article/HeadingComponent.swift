56d32e69ecaa7d59a17f9615482d3b8a69a4941a
Jacob Morris <jmorris@mozilla.com>
2021-12-23 14:46:10 -0800

    Don't prepend hashes to heading content
    Backend team is properly including the hashes now.

--- PocketKit/Sources/Sync/Article/HeadingComponent.swift
+++ PocketKit/Sources/Sync/Article/HeadingComponent.swift
@@ -1,23 +1,6 @@
 public struct HeadingComponent: MarkdownComponent, Codable, Equatable, Hashable {
-    private let _content: Markdown
+    public let content: Markdown
     public let level: UInt
-
-    public var content: Markdown {
-        return [
-            String(repeating: "#", count: Int(level)),
-            _content
-        ].joined(separator: " ")
-    }
-
-    enum CodingKeys: String, CodingKey {
-        case _content = "content"
-        case level
-    }
-
-    init(content: String, level: UInt) {
-        self._content = content
-        self.level = level
-    }
 }
 
 extension HeadingComponent {
