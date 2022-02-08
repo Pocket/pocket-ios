d61abf860efb179c00534058aa049ce82a73ca50
Jacob Morris <jmorris@mozilla.com>
2022-02-07 19:25:38 -0800

    Support (un)favoriting archived items from reader
    Co-authored-by: Jacob Morris <jmorris@mozilla.com>

--- PocketKit/Sources/PocketKit/PocketAlert.swift
+++ PocketKit/Sources/PocketKit/PocketAlert.swift
@@ -8,3 +8,19 @@ struct PocketAlert {
     let actions: [UIAlertAction]
     let preferredAction: UIAlertAction?
 }
+
+extension PocketAlert {
+    init(_ error: Error, handler: @escaping () -> Void) {
+        self.init(
+            title: "An error occurred",
+            message: error.localizedDescription,
+            preferredStyle: .alert,
+            actions: [
+                UIAlertAction(title: "Ok", style: .default) { _ in
+                    handler()
+                }
+            ],
+            preferredAction: nil
+        )
+    }
+}
