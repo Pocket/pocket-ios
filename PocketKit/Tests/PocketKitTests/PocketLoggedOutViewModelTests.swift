4986f35b0dde72a17f69fd1e873ba1d39d4d3a81
David Skuza <dskuza@getpocket.com>
2022-02-23 16:18:56 -0600

    Add support for signing up via FxA

--- PocketKit/Tests/PocketKitTests/PocketLoggedOutViewModelTests.swift
+++ PocketKit/Tests/PocketKitTests/PocketLoggedOutViewModelTests.swift
@@ -20,7 +20,9 @@ class PocketLoggedOutViewModelTests: XCTestCase {
     override func tearDown() {
         subscriptions = []
     }
+}
 
+extension PocketLoggedOutViewModelTests {
     func test_logIn_onGUIDError_setsPresentedAlert() {
         let urlSession = MockURLSession()
         urlSession.stubData { _ in
@@ -103,6 +105,93 @@ class PocketLoggedOutViewModelTests: XCTestCase {
         }.store(in: &subscriptions)
 
         viewModel.logIn()
+        wait(for: [sessionExpectation], timeout: 1)
+    }
+}
+
+extension PocketLoggedOutViewModelTests {
+    func test_signUp_onGUIDError_setsPresentedAlert() {
+        let urlSession = MockURLSession()
+        urlSession.stubData { _ in
+            throw FakeError.error
+        }
+
+        let client = AuthorizationClient(
+            consumerKey: "test-consumer-key",
+            urlSession: urlSession,
+            authenticationSession: MockErrorAuthenticationSession.self
+        )
+
+        viewModel = PocketLoggedOutViewModel(
+            authorizationClient: client,
+            appSession: appSession
+        )
+        viewModel.contextProvider = self
+
+        let alertExpectation = expectation(description: "set presented alert")
+        viewModel.$presentedAlert.dropFirst().sink { alert in
+            XCTAssertNotNil(alert)
+            alertExpectation.fulfill()
+        }.store(in: &subscriptions)
+
+        viewModel.signUp()
+        wait(for: [alertExpectation], timeout: 1)
+    }
+
+    func test_signUp_onFxAError_setsPresentedAlert() {
+        let urlSession = MockURLSession()
+        urlSession.stubData { request in
+            self.validGUIDResponse(for: request)
+        }
+
+        let client = AuthorizationClient(
+            consumerKey: "test-consumer-key",
+            urlSession: urlSession,
+            authenticationSession: MockErrorAuthenticationSession.self
+        )
+        viewModel = PocketLoggedOutViewModel(
+            authorizationClient: client,
+            appSession: appSession
+        )
+
+        viewModel.contextProvider = self
+
+        let alertExpectation = expectation(description: "set presented alert")
+        viewModel.$presentedAlert.dropFirst().sink { alert in
+            XCTAssertNotNil(alert)
+            alertExpectation.fulfill()
+        }.store(in: &subscriptions)
+
+        viewModel.signUp()
+        wait(for: [alertExpectation], timeout: 1)
+    }
+
+    func test_signUp_onFxASuccess_updatesSession() {
+        let urlSession = MockURLSession()
+        urlSession.stubData { request in
+            self.validGUIDResponse(for: request)
+        }
+
+        let client = AuthorizationClient(
+            consumerKey: "test-consumer-key",
+            urlSession: urlSession,
+            authenticationSession: MockAuthenticationSession.self
+        )
+        viewModel = PocketLoggedOutViewModel(
+            authorizationClient: client,
+            appSession: appSession
+        )
+        viewModel.contextProvider = self
+
+        let sessionExpectation = expectation(description: "published error event")
+        appSession.$currentSession.dropFirst().sink { session in
+            XCTAssertEqual(session?.guid, "sample-guid")
+            XCTAssertEqual(session?.accessToken, "test-access-token")
+            XCTAssertEqual(session?.userIdentifier, "")
+            sessionExpectation.fulfill()
+        }.store(in: &subscriptions)
+
+        viewModel.signUp()
         wait(for: [sessionExpectation], timeout: 1)
     }
 }
