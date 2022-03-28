522f422d89554b6f569a662da6d1fc9615f7879e
David Skuza <dskuza@getpocket.com>
2022-03-28 15:33:36 -0500

    SaveToPocket: Show error if there is no current session

--- PocketKit/Sources/SaveToPocketKit/MainViewController.swift
+++ PocketKit/Sources/SaveToPocketKit/MainViewController.swift
@@ -8,7 +8,7 @@ import Sync
 class MainViewController: UIViewController {
     private let imageView = UIImageView(image: UIImage(asset: .logo))
 
-    private let capsuleView = MainCapsuleView()
+    private let infoView = MainInfoView()
 
     private let dismissLabel = UILabel()
 
@@ -17,11 +17,14 @@ class MainViewController: UIViewController {
     convenience init() {
         Textiles.initialize()
 
+        let appSession = AppSession()
+
         self.init(
             viewModel: MainViewModel(
+                appSession: appSession,
                 saveService: PocketSaveService(
                     apollo: ApolloClient.createDefault(
-                        sessionProvider: AppSession(),
+                        sessionProvider: appSession,
                         consumerKey: Keys.shared.pocketApiConsumerKey
                     ),
                     backgroundActivityPerformer: ProcessInfo.processInfo,
@@ -47,15 +50,15 @@ class MainViewController: UIViewController {
         view.backgroundColor = UIColor(.ui.white1)
 
         view.addSubview(imageView)
-        view.addSubview(capsuleView)
+        view.addSubview(infoView)
         view.addSubview(dismissLabel)
 
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        capsuleView.translatesAutoresizingMaskIntoConstraints = false
+        infoView.translatesAutoresizingMaskIntoConstraints = false
         dismissLabel.translatesAutoresizingMaskIntoConstraints = false
 
         let capsuleTopConstraint = NSLayoutConstraint(
-            item: capsuleView,
+            item: infoView,
             attribute: .top,
             relatedBy: .equal,
             toItem: view,
@@ -69,23 +72,30 @@ class MainViewController: UIViewController {
             imageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 36),
 
             capsuleTopConstraint,
-            capsuleView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            capsuleView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
-            capsuleView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24),
+            infoView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            infoView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
+            infoView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24),
 
             dismissLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             dismissLabel.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
 
-        capsuleView.attributedText = NSAttributedString(string: "Saved to Pocket", style: .mainText)
         dismissLabel.attributedText = NSAttributedString(string: "Tap to Dismiss", style: .dismiss)
 
         let tap = UITapGestureRecognizer(target: self, action: #selector(finish))
         view.addGestureRecognizer(tap)
 
+        updateUI()
+
         Task {
             await viewModel.save(from: extensionContext)
         }
+    }
+
+    private func updateUI() {
+        infoView.style = viewModel.style
+        infoView.attributedText = viewModel.attributedText
+        infoView.attributedDetailText = viewModel.attributedDetailText
     }
 
     @objc
@@ -94,54 +104,6 @@ class MainViewController: UIViewController {
     }
 }
 
-private class MainCapsuleView: UIView {
-    private let imageView = UIImageView(image: UIImage(asset: .circleChecked))
-
-    private let textLabel = UILabel()
-
-    private lazy var stackView: UIStackView = {
-        let stackView = UIStackView(arrangedSubviews: [imageView, textLabel])
-        stackView.spacing = 12
-        stackView.axis = .horizontal
-        return stackView
-    }()
-
-    var attributedText: NSAttributedString? {
-        get { textLabel.attributedText }
-        set { textLabel.attributedText = newValue }
-    }
-
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-
-        backgroundColor = UIColor(.ui.teal6)
-
-        addSubview(stackView)
-
-        imageView.setContentCompressionResistancePriority(.required, for: .horizontal)
-        imageView.setContentCompressionResistancePriority(.required, for: .vertical)
-        textLabel.adjustsFontSizeToFitWidth = true
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            stackView.topAnchor.constraint(equalTo: topAnchor, constant: 5),
-            stackView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -5),
-            stackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
-            stackView.trailingAnchor.constraint(lessThanOrEqualTo: trailingAnchor, constant: -16)
-        ])
-    }
-
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    override func layoutSubviews() {
-        super.layoutSubviews()
-
-        layer.cornerRadius = bounds.size.height / 2
-    }
-}
-
 private extension Style {
-    static let mainText: Self = .header.sansSerif.h2.with(color: .ui.teal2)
-    static let dismiss: Self = .header.sansSerif.p3.with(color: .ui.grey5)
+    static let dismiss: Self = .header.sansSerif.p3.with(color: .ui.grey5).with { $0.with(lineHeight: .explicit(22)) }
 }
