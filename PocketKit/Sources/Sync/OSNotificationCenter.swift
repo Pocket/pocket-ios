fdda57f0552354f2bc7f9e11921f68abcd68ce98
Jacob Morris <jmorris@mozilla.com>
2022-04-01 17:38:54 -0700

    Sync: Publish an event when other process(es) send notifications
    - savedItemCreated: Published by share extension when a new item is
      saved
    - savedItemUpdated: Published by share extension when an item is updated
      Metadata for this notification is stored in the shared core data
      container so that the container app can access information about which
      item was updated.
    
    https://getpocket.atlassian.net/browse/IN-325
    
    Co-authored-by: David Skuza <dskuza@getpocket.com>

---
+++ PocketKit/Sources/Sync/OSNotificationCenter.swift
@@ -0,0 +1,108 @@
+import Foundation
+import CoreFoundation
+
+
+public class OSNotificationCenter {
+    public typealias Observer = AnyHashable
+    private let center: CFNotificationCenter
+    private let storage: Storage
+
+    public init(notifications: CFNotificationCenter) {
+        self.center = notifications
+        self.storage = Storage()
+    }
+
+    deinit {
+        storage.forEach { observer, notificationName, handler in
+            CFNotificationCenterRemoveObserver(center, handler.pointer, notificationName, nil)
+        }
+    }
+
+    public func add(observer: Observer, name: CFNotificationName, handler: @escaping () -> Void) {
+        let notificationHandler = NotificationHandler(handler: handler)
+        storage.add(observer: observer, name: name, handler: notificationHandler)
+
+        CFNotificationCenterAddObserver(
+            center,
+            notificationHandler.pointer,
+            notificationHandler.callback,
+            name.rawValue,
+            nil,
+            .deliverImmediately
+        )
+    }
+
+    public func remove(observer: Observer, name: CFNotificationName) {
+        storage
+            .handlers(for: observer, name: name)?
+            .forEach { handler in
+                CFNotificationCenterRemoveObserver(center, handler.pointer, name, nil)
+            }
+
+        storage.remove(observer: observer, name: name)
+    }
+
+    public func post(name: CFNotificationName) {
+        CFNotificationCenterPostNotification(center, name, nil, nil, true)
+    }
+
+    private class Storage {
+        private struct Key: Hashable {
+            let observer: Observer
+            let notificationName: CFNotificationName
+
+            init(_ observer: Observer, _ notificationName: CFNotificationName) {
+                self.observer = observer
+                self.notificationName = notificationName
+            }
+        }
+
+        private var _handlers: [Key: [NotificationHandler]] = [:]
+
+        func add(observer: Observer, name: CFNotificationName, handler: NotificationHandler) {
+            let key = Key(observer, name)
+            _handlers[key] = (_handlers[key] ?? []) + [handler]
+        }
+
+        func remove(observer: Observer, name: CFNotificationName) {
+            _handlers[Key(observer, name)] = nil
+        }
+
+        func handlers(for observer: Observer, name: CFNotificationName) -> [NotificationHandler]? {
+            _handlers[Key(observer, name)]
+        }
+
+        func forEach(closure: (Observer, CFNotificationName, NotificationHandler) -> Void) {
+            _handlers.forEach { key, handlers in
+                handlers.forEach { handler in
+                    closure(key.observer, key.notificationName, handler)
+                }
+            }
+        }
+    }
+
+    private class NotificationHandler {
+        private let handler: () -> Void
+
+        init(handler: @escaping () -> Void) {
+            self.handler = handler
+        }
+
+        var pointer: UnsafeMutableRawPointer {
+            Unmanaged<NotificationHandler>.passUnretained(self).toOpaque()
+        }
+
+        var callback: CFNotificationCallback {
+            { _, pointer, _, _, _ in
+                pointer.flatMap {
+                    Unmanaged<NotificationHandler>.fromOpaque($0).takeUnretainedValue()
+                }?.handler()
+            }
+        }
+    }
+}
+
+public extension CFNotificationName {
+    static let savedItemCreated = CFNotificationName("com.mozilla.pocket.savedItemCreated" as CFString)
+    static let savedItemUpdated = CFNotificationName("com.mozilla.pocket.savedItemUpdated" as CFString)
+}
---
+++ PocketKit/Tests/SyncTests/OSNotificationCenterTests.swift
@@ -0,0 +1,146 @@
+import XCTest
+import Foundation
+
+@testable import Sync
+
+
+class OSNotificationCenterTests: XCTestCase {
+    var cfCenter: CFNotificationCenter!
+
+    override func setUp() {
+        cfCenter = CFNotificationCenterGetDarwinNotifyCenter()
+    }
+
+    func test_register_registersGivenObserverWithGivenHandler() {
+        let center = OSNotificationCenter(notifications: cfCenter)
+
+        let notificationWasHandled = expectation(description: "notificationWasHandled")
+        center.add(observer: self, name: .testNotification) {
+            notificationWasHandled.fulfill()
+        }
+
+        CFNotificationCenterPostNotification(cfCenter, .testNotification, nil, nil, true)
+        wait(for: [notificationWasHandled], timeout: 1)
+    }
+
+    func test_register_whenAlreadyRegistered_addsBothObservers() {
+        let center = OSNotificationCenter(notifications: cfCenter)
+
+        let notificationWasHandled = expectation(description: "notificationWasHandled")
+        center.add(observer: self, name: .testNotification) {
+            notificationWasHandled.fulfill()
+        }
+
+        let notificationWasHandled2 = expectation(description: "notificationWasHandled2")
+        center.add(observer: self, name: .testNotification) {
+            notificationWasHandled2.fulfill()
+        }
+
+        CFNotificationCenterPostNotification(cfCenter, .testNotification, nil, nil, true)
+        wait(for: [notificationWasHandled, notificationWasHandled2], timeout: 1)
+    }
+
+    func test_remove_removesGivenObserverForGivenNotification() {
+        let center = OSNotificationCenter(notifications: cfCenter)
+
+        let observers = [UUID(), UUID()]
+        center.add(observer: observers[0], name: .testNotification) {
+            XCTFail("Observer 1 should not be notified after being removed")
+        }
+
+        let observer2HandledNotification = expectation(description: "observer2HandledNotification")
+        center.add(observer: observers[1], name: .testNotification) {
+            observer2HandledNotification.fulfill()
+        }
+
+        center.remove(observer: observers[0], name: .testNotification)
+
+        CFNotificationCenterPostNotification(cfCenter, .testNotification, nil, nil, true)
+        wait(for: [observer2HandledNotification], timeout: 1)
+    }
+
+    func test_remove_whenObservingWithMultipleHandlers_removesAllHandlers() {
+        let center = OSNotificationCenter(notifications: cfCenter)
+
+        center.add(observer: self, name: .testNotification) {
+            XCTFail("Should not be notified after removing")
+        }
+        center.add(observer: self, name: .testNotification) {
+            XCTFail("Should not be notified after removing")
+        }
+        center.remove(observer: self, name: .testNotification)
+
+        CFNotificationCenterPostNotification(cfCenter, .testNotification, nil, nil, true)
+
+        // In order to trigger a proper failure, we need to give the OS some time
+        // to invoke notification handlers.
+        let pause = expectation(description: "pause")
+        DispatchQueue.global(qos: .background).asyncAfter(deadline: .now() + 1) {
+            pause.fulfill()
+        }
+        wait(for: [pause], timeout: 2)
+    }
+
+    func test_remove_doesNotRemoveHandlerForOtherNotifications() {
+        let center = OSNotificationCenter(notifications: cfCenter)
+
+        center.add(observer: self, name: .testNotification) {
+            XCTFail("Observer 1 should not be notified of `testNotification` after being removed")
+        }
+
+        let wasNotifiedOfAnotherTestNotification = expectation(description: "wasNotifiedOfAnotherTestNotification")
+        center.add(observer: self, name: .anotherTestNotification) {
+            wasNotifiedOfAnotherTestNotification.fulfill()
+        }
+
+        center.remove(observer: self, name: .testNotification)
+
+        CFNotificationCenterPostNotification(cfCenter, .testNotification, nil, nil, true)
+        CFNotificationCenterPostNotification(cfCenter, .anotherTestNotification, nil, nil, true)
+
+        wait(for: [wasNotifiedOfAnotherTestNotification], timeout: 1)
+    }
+
+    func test_remove_doesNotRetainObservers() {
+        class Observer: NSObject {}
+
+        let center = OSNotificationCenter(notifications: cfCenter)
+        weak var weakObserver: Observer?
+        autoreleasepool {
+            var observer: Observer? = Observer()
+            weakObserver = observer
+
+            center.add(observer: observer, name: .testNotification) {
+                XCTFail("Observer 1 should not be notified of `testNotification` after being removed")
+            }
+
+            center.remove(observer: observer, name: .testNotification)
+
+            observer = nil
+        }
+
+        XCTAssertNil(weakObserver)
+    }
+
+    func test_add_retainsObservers() {
+        class Observer: NSObject {}
+
+        let center = OSNotificationCenter(notifications: cfCenter)
+        let receivedNotification = expectation(description: "receivedNotification")
+
+        autoreleasepool {
+            let observer: Observer = Observer()
+            center.add(observer: observer, name: .testNotification) {
+                receivedNotification.fulfill()
+            }
+        }
+
+        CFNotificationCenterPostNotification(cfCenter, .testNotification, nil, nil, true)
+        wait(for: [receivedNotification], timeout: 1)
+    }
+}
+
+extension CFNotificationName {
+    static let testNotification: CFNotificationName = .init(rawValue: "testNotification" as CFString)
+    static let anotherTestNotification: CFNotificationName = .init(rawValue: "anotherTestNotification" as CFString)
+}
