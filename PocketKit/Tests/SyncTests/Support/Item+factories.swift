9450edf9fc57769bb6c22c2b6ba719d496f086a9
David Skuza <dskuza@getpocket.com>
2022-04-19 10:22:25 -0500

    Rename Core Data id properties to remoteID

--- PocketKit/Tests/SyncTests/Support/Item+factories.swift
+++ PocketKit/Tests/SyncTests/Support/Item+factories.swift
@@ -4,10 +4,12 @@
 extension Item {
     static func build(
         in space: Space = Space(container: .testContainer),
-        remoteID: String = "item-1"
+        remoteID: String? = "item-1",
+        title: String? = "Item 1 Title"
     ) -> Item {
         let item: Item = space.new()
         item.remoteID = remoteID
+        item.title = title
         return item
     }
 }
