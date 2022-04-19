d23a18b2a257d966d9cfaa96ef95781997405c4f
David Skuza <dskuza@getpocket.com>
2022-04-19 12:24:54 -0500

    Transition fetchSlate to use Core Data

--- PocketKit/Sources/Sync/Slates/SlateService.swift
+++ PocketKit/Sources/Sync/Slates/SlateService.swift
@@ -25,7 +25,7 @@ class APISlateService: SlateService {
             return
         }
 
-        if let existingSlateLineup = try space.fetchSlateLineup(byID: remote.id) {
+        if let existingSlateLineup = try space.fetchSlateLineup(byRemoteID: remote.id) {
             space.delete(existingSlateLineup)
         }
 
@@ -34,10 +34,24 @@ class APISlateService: SlateService {
         let slateLineup: SlateLineup = space.new()
         slateLineup.update(from: remote, in: space)
 
-        try space.save()
+        try self.space.save()
     }
 
     func fetchSlate(_ slateID: String) async throws {
-        
+        let query = GetSlateQuery(slateID: slateID, recommendationCount: 25)
+
+        guard let remote = try await apollo.fetch(query: query).data?.getSlate?.fragments.slateParts else {
+            return
+        }
+
+        let unsavedItems = try space.fetchUnsavedItems()
+        unsavedItems.forEach {
+            space.delete($0)
+        }
+
+        let slate: Slate = try space.fetchOrCreateSlate(byRemoteID: remote.id)
+        slate.update(from: remote, in: space)
+
+        try self.space.save()
     }
 }
