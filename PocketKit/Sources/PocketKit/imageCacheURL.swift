54a5cf27bf5bb87d57e457b2e353b0239d2e2e76
Jacob Morris <jmorris@mozilla.com>
2022-05-13 10:24:24 -0700

    Include thumbor filters in image cache requests
    https://getpocket.atlassian.net/browse/IN-564

--- PocketKit/Sources/PocketKit/imageCacheURL.swift
+++ PocketKit/Sources/PocketKit/imageCacheURL.swift
@@ -1,13 +1,18 @@
-import Foundation
+import UIKit
 
-
-private let imageCacheBaseURL: URL = URL(string: "https://pocket-image-cache.com")!
-
+private let imageCacheFilters = "/fit-in/\(Int(UIScreen.main.nativeBounds.width))x\(Int(UIScreen.main.nativeBounds.height))/filters:format(jpeg):quality(60):no_upscale():strip_exif()"
+private let imageCacheBaseURLComponents = URLComponents(string: "https://pocket-image-cache.com")!
 
 func imageCacheURL(for imageURL: URL?) -> URL? {
-    imageURL.flatMap {
-        $0.absoluteString.addingPercentEncoding(withAllowedCharacters: .alphanumerics)
-    }.flatMap {
-        return URL(string: $0, relativeTo: imageCacheBaseURL)
-    }
+    imageURL
+        .flatMap { $0.absoluteString }
+        .flatMap { imageURLString in
+            var components = imageCacheBaseURLComponents
+            components.path = [
+                imageCacheFilters,
+                imageURLString
+            ].joined(separator: "/")
+
+            return components.url
+        }
 }
