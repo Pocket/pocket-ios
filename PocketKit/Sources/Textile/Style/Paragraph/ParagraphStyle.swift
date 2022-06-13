b504fd5bee2c271581433012b2f708eb07de4a36
David Skuza <dskuza@getpocket.com>
2022-06-13 10:41:09 -0500

    Textile: support vertical alignment of attributed strings
    - Supports "default" and "center" vertical alignment of text when used with attributed strings.
    
    During design review, the SaveTo extension had incorrect padding when multi-line text was visible in the capsule. It was determined that the difference is due to how Figma uses the CSS "line-height" property vs how iOS renders text. "line-height" will center text within the line height, whereas iOS will pin text to the bottom. We can mimic "line-height" behavior by calculating and setting a baseline offset for an attributed string, which allows us to better match Figma designs.

--- PocketKit/Sources/Textile/Style/Paragraph/ParagraphStyle.swift
+++ PocketKit/Sources/Textile/Style/Paragraph/ParagraphStyle.swift
@@ -16,32 +16,39 @@ public struct ParagraphStyle {
     public let lineBreakMode: LineBreakMode
     public let lineSpacing: CGFloat?
     public let lineHeight: LineHeight?
+    public let verticalAlignment: VerticalTextAlignment
 
     public init(
         alignment: TextAlignment,
         lineBreakMode: LineBreakMode = .none,
         lineSpacing: CGFloat? = nil,
-        lineHeight: LineHeight? = nil
+        lineHeight: LineHeight? = nil,
+        verticalAlignment: VerticalTextAlignment = .default
     ) {
         self.alignment = alignment
         self.lineBreakMode = lineBreakMode
         self.lineSpacing = lineSpacing
         self.lineHeight = lineHeight
+        self.verticalAlignment = verticalAlignment
     }
 
     public func with(alignment: TextAlignment) -> ParagraphStyle {
-        return ParagraphStyle(alignment: alignment, lineBreakMode: lineBreakMode, lineSpacing: lineSpacing, lineHeight: lineHeight)
+        return ParagraphStyle(alignment: alignment, lineBreakMode: lineBreakMode, lineSpacing: lineSpacing, lineHeight: lineHeight, verticalAlignment: verticalAlignment)
     }
 
     public func with(lineBreakMode: LineBreakMode) -> ParagraphStyle {
-        return ParagraphStyle(alignment: alignment, lineBreakMode: lineBreakMode, lineSpacing: lineSpacing, lineHeight: lineHeight)
+        return ParagraphStyle(alignment: alignment, lineBreakMode: lineBreakMode, lineSpacing: lineSpacing, lineHeight: lineHeight, verticalAlignment: verticalAlignment)
     }
 
     public func with(lineSpacing: CGFloat?) -> ParagraphStyle {
-        return ParagraphStyle(alignment: alignment, lineBreakMode: lineBreakMode, lineSpacing: lineSpacing, lineHeight: lineHeight)
+        return ParagraphStyle(alignment: alignment, lineBreakMode: lineBreakMode, lineSpacing: lineSpacing, lineHeight: lineHeight, verticalAlignment: verticalAlignment)
     }
 
     public func with(lineHeight: LineHeight?) -> ParagraphStyle {
-        return ParagraphStyle(alignment: alignment, lineBreakMode: lineBreakMode, lineSpacing: lineSpacing, lineHeight: lineHeight)
+        return ParagraphStyle(alignment: alignment, lineBreakMode: lineBreakMode, lineSpacing: lineSpacing, lineHeight: lineHeight, verticalAlignment: verticalAlignment)
+    }
+
+    public func with(verticalAlignment: VerticalTextAlignment) -> ParagraphStyle {
+        return ParagraphStyle(alignment: alignment, lineBreakMode: lineBreakMode, lineSpacing: lineSpacing, lineHeight: lineHeight, verticalAlignment: verticalAlignment)
     }
 }
