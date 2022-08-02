d6f437b7b9f1ba84283791dc5fae26f155e81a09
Jacob Morris <jmorris@mozilla.com>
2022-08-01 17:36:17 -0700

    SlateService: handle responses on main thread

--- PocketKit/Sources/Sync/Slates/SlateService.swift
+++ PocketKit/Sources/Sync/Slates/SlateService.swift
@@ -20,7 +20,6 @@ class APISlateService: SlateService {
         self.space = space
     }
 
-    @MainActor
     func fetchSlateLineup(_ identifier: String) async throws {
         let query = GetSlateLineupQuery(lineupID: identifier, maxRecommendations: 5)
 
@@ -28,6 +27,22 @@ class APISlateService: SlateService {
             return
         }
 
+        try await handle(remote: remote)
+    }
+
+    func fetchSlate(_ slateID: String) async throws {
+        let query = GetSlateQuery(slateID: slateID, recommendationCount: 25)
+
+        guard let remote = try await apollo.fetch(query: query)
+            .data?.getSlate?.fragments.slateParts else {
+            return
+        }
+
+        try await handle(remote: remote)
+    }
+
+    @MainActor
+    private func handle(remote: GetSlateLineupQuery.Data.GetSlateLineup) throws {
         let lineup = try space.fetchSlateLineup(byRemoteID: remote.id) ?? space.new()
         lineup.update(from: remote, in: space)
 
@@ -37,17 +52,10 @@ class APISlateService: SlateService {
     }
 
     @MainActor
-    func fetchSlate(_ slateID: String) async throws {
-        let query = GetSlateQuery(slateID: slateID, recommendationCount: 25)
-
-        guard let remote = try await apollo.fetch(query: query)
-            .data?.getSlate?.fragments.slateParts else {
-            return
-        }
-
-        let slate: Slate = try space.fetchOrCreateSlate(byRemoteID: remote.id)
+    private func handle(remote: SlateParts) throws {
+        let slate = try space.fetchOrCreateSlate(byRemoteID: remote.id)
         slate.update(from: remote, in: space)
 
-        try self.space.save()
+        try space.save()
     }
 }
