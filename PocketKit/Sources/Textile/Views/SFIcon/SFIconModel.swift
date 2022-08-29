a57589d09fdc2e28b65c0c3d32e2e4347337dfcb
Ky Hamilton <8561013+kybhamilton@users.noreply.github.com>
2022-08-29 17:48:07 -0600

    IN-778: SETTINGS SCREEN
    Created universal SFIcon component to make it easier to use SF Symbols in SwiftUI.

---
+++ PocketKit/Sources/Textile/Views/SFIcon/SFIconModel.swift
@@ -0,0 +1,20 @@
+import Foundation
+import SwiftUI
+
+
+public class SFIconModel: ObservableObject {
+    
+    var systemImage: String
+    var size: CGFloat
+    var weight: Font.Weight
+    var rotation: CGFloat
+    var color: Color
+    
+    public init(_ systemImage: String, size: CGFloat = 18, weight: Font.Weight = .regular, rotation: CGFloat = 0, color: Color = Color(.ui.black)) {
+        self.systemImage = systemImage
+        self.size = size
+        self.weight = weight
+        self.rotation = rotation
+        self.color = color
+    }
+}
