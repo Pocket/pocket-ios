f5dbf16a0b11dd0412cecfd91b3fe0341df47ef5
Ky Hamilton <8561013+kybhamilton@users.noreply.github.com>
2022-08-30 13:43:25 -0600

    IN-778: SETTINGS SCREEN
    Updated settings to use black1 to support dark mode and added some conditional styling to the PremiumRow.

--- PocketKit/Sources/PocketKit/Settings/Components/SettingsRowButton.swift
+++ PocketKit/Sources/PocketKit/Settings/Components/SettingsRowButton.swift
@@ -6,7 +6,7 @@ struct SettingsRowButton: View {
     var title: String
     var titleStyle: Style = .settings.row.default
     var icon: SFIconModel? = nil
-    var imageColor: Color = Color(.ui.black)
+    var imageColor: Color = Color(.ui.black1)
     
     let action: () -> Void
     
