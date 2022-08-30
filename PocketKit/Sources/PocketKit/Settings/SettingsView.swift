4219110e73a0de83ca6c5d9c16fc901faadd59f5
Ky Hamilton <8561013+kybhamilton@users.noreply.github.com>
2022-08-30 15:06:41 -0600

    IN-778: SETTINGS SCREEN
    Set PremiumRow to .notSubscribed.

--- PocketKit/Sources/PocketKit/Settings/SettingsView.swift
+++ PocketKit/Sources/PocketKit/Settings/SettingsView.swift
@@ -23,7 +23,7 @@ struct SettingsView: View {
         VStack(spacing:0) {
             Form {
                 Section(header: Text("Your Account").style(.settings.header)) {
-                    PremiumRow(status: .subscribed, destination: EmptyView())
+                    PremiumRow(status: .notSubscribed, destination: EmptyView())
                     SettingsRowLink(title: "Reset Password", destination: EmptyView())
                     SettingsRowLink(title: "Delete Account", destination: EmptyView())
                 }.textCase(nil)
