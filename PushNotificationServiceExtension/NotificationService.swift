1c34ced62ffcdc24fc8a0a1ada7608cd4bb0af08
Daniel Brooks <bass_rock@me.com>
2022-10-12 08:46:13 -0700

    fix(braze): silence push when logged out

--- PushNotificationServiceExtension/NotificationService.swift
+++ PushNotificationServiceExtension/NotificationService.swift
@@ -3,5 +3,36 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import BrazeNotificationService
+import SharedPocketKit
+import UserNotifications
 
-class NotificationService: BrazeNotificationService.NotificationService {}
+class NotificationService: BrazeNotificationService.NotificationService {
+
+    /**
+     Pocket App session to determine if logged in.
+     */
+    let appSession: AppSession
+
+    override init() {
+         appSession = AppSession()
+        super.init()
+    }
+
+    public override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+        guard let _ = appSession.currentSession else {
+            contentHandler(UNNotificationContent())
+            return
+        }
+
+        //Forward notification request to Braze
+        if brazeHandle(request: request, contentHandler: contentHandler) {
+         // Braze handled the notification, nothing more to do.
+         return
+        }
+
+        // Braze did not handle this notification request,
+        // manually call the content handler to let the system
+        // know that the user notification is processed.
+        contentHandler(request.content)
+    }
+}
