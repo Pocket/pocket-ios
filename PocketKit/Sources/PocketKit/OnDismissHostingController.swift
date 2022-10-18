93f2c6c099b3b04caeb9776412316f20a96d96df
cyndichin <cgc95@cornell.edu>
2022-10-18 11:09:45 -0400

    chore(swiftlint): Fix attributes warnings

--- PocketKit/Sources/PocketKit/OnDismissHostingController.swift
+++ PocketKit/Sources/PocketKit/OnDismissHostingController.swift
@@ -14,7 +14,8 @@ class OnDismissHostingController<T: View>: UIHostingController<T> {
         }
     }
 
-    @MainActor @objc required dynamic init?(coder aDecoder: NSCoder) {
+    @MainActor @objc
+    required dynamic init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
--- PocketKit/Sources/PocketKit/ReaderSettings/ReaderSettingsViewController.swift
+++ PocketKit/Sources/PocketKit/ReaderSettings/ReaderSettingsViewController.swift
@@ -8,7 +8,8 @@ class ReaderSettingsViewController: OnDismissHostingController<ReaderSettingsVie
         )
     }
 
-    @MainActor @objc required dynamic init?(coder aDecoder: NSCoder) {
+    @MainActor @objc
+    required dynamic init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
