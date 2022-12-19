dde5873da8282796f382097e6d0dd8b829d3a5b7
cyndichin <cgc95@cornell.edu>
2022-12-19 11:43:32 -0500

    refactor(search): rewrote ItemsListItemCell in SwiftUI

---
+++ PocketKit/Sources/PocketKit/MyList/ItemsList/SwiftUI/Components/ActionButton.swift
@@ -0,0 +1,26 @@
+import SwiftUI
+import Textile
+
+struct ActionButton: View {
+    var itemAction: ItemAction?
+    var selected: Bool = false
+    var trailingPadding: Bool = true
+
+    init(_ itemAction: ItemAction?, selected: Bool = false, trailingPadding: Bool = true) {
+        self.itemAction = itemAction
+        self.selected = selected
+        self.trailingPadding = trailingPadding
+    }
+
+    var body: some View {
+        if let action = itemAction, let handler = action.handler, let image = action.image {
+            Button {
+                handler {}
+            } label: {
+                Image(uiImage: image)
+                    .actionButtonStyle(selected: selected, trailingPadding: trailingPadding)
+                    .accessibilityIdentifier(action.accessibilityIdentifier)
+            }
+        }
+    }
+}
