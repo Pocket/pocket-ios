2355ebdea288b5709c69cc4acb2ab0bac1fef299
David Skuza <dskuza@getpocket.com>
2023-01-10 14:23:26 -0600

    project(spm): use exact dependency versions

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.4
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -10,10 +10,16 @@ let package = Package(
         .library(name: "DangerDeps", type: .dynamic, targets: ["DangerDependencies"]), // dev
     ],
     dependencies: [
-        .package(name: "Danger", url: "https://github.com/danger/swift.git", from: "3.14.2"), // dev
-        .package(name: "DangerSwiftCoverage", url: "https://github.com/f-meloni/danger-swift-coverage", from: "1.2.1") // dev
+        .package(url: "https://github.com/danger/swift.git", exact: "3.15.0"), // dev
+        .package(url: "https://github.com/f-meloni/danger-swift-coverage", exact: "1.2.1") // dev
     ],
     targets: [
-        .target(name: "DangerDependencies", dependencies: ["Danger", "DangerSwiftCoverage"]), // dev
+        .target(
+            name: "DangerDependencies",
+            dependencies: [
+                .product(name: "Danger", package: "swift"),
+                .product(name: "DangerSwiftCoverage", package: "danger-swift-coverage")
+            ]
+        ) // dev
     ]
 )
--- PocketKit/Package.swift
+++ PocketKit/Package.swift
@@ -15,14 +15,14 @@ let package = Package(
         .library(name: "Analytics", targets: ["Analytics"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apollographql/apollo-ios.git", .upToNextMajor(from: "1.0.5")),
-        .package(url: "https://github.com/onevcat/Kingfisher.git", .upToNextMajor(from: "7.4.1")),
-        .package(url: "https://github.com/getsentry/sentry-cocoa.git", .upToNextMajor(from: "7.31.5")),
-        .package(url: "https://github.com/snowplow/snowplow-objc-tracker", .upToNextMajor(from: "4.0.1")),
-        .package(url: "https://github.com/airbnb/lottie-ios.git", from: "3.5.0"),
-        .package(url: "https://github.com/johnxnguyen/Down", .upToNextMinor(from: "0.11.0")),
-        .package(url: "https://github.com/SvenTiigi/YouTubePlayerKit.git", .upToNextMinor(from: "1.1.5")),
-        .package(url: "https://github.com/braze-inc/braze-swift-sdk.git", .upToNextMajor(from: "5.6.4"))
+        .package(url: "https://github.com/apollographql/apollo-ios.git", exact: "1.0.5"),
+        .package(url: "https://github.com/onevcat/Kingfisher.git", exact: "7.4.1"),
+        .package(url: "https://github.com/getsentry/sentry-cocoa.git", exact: "7.31.5"),
+        .package(url: "https://github.com/snowplow/snowplow-objc-tracker", exact: "4.1.0"),
+        .package(url: "https://github.com/airbnb/lottie-ios.git", exact: "3.5.0"),
+        .package(url: "https://github.com/johnxnguyen/Down", exact: "0.11.0"),
+        .package(url: "https://github.com/SvenTiigi/YouTubePlayerKit.git", exact: "1.1.12"),
+        .package(url: "https://github.com/braze-inc/braze-swift-sdk.git", exact: "5.8.1")
     ],
     targets: [
         .target(
