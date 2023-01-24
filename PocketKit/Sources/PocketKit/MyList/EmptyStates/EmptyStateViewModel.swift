1278b1739407255fa48cdc7c11fd8510463bcb13
cyndichin <cgc95@cornell.edu>
2023-01-24 12:49:22 -0500

    feat(search): search empty states

--- PocketKit/Sources/PocketKit/MyList/EmptyStates/EmptyStateViewModel.swift
+++ PocketKit/Sources/PocketKit/MyList/EmptyStates/EmptyStateViewModel.swift
@@ -4,6 +4,7 @@ import Textile
 
 protocol EmptyStateViewModel {
     var imageAsset: ImageAsset { get }
+    var maxWidth: CGFloat { get }
     var icon: ImageAsset? { get }
     var headline: String { get }
     var detailText: String? { get }
