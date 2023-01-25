eae0ef3b571e530e7fd7842c332ebdf497f3c672
timc-mozilla <tchoh@mozilla.com>
2023-01-25 10:16:48 -0600

    fixing failed test

--- PocketKit/Tests/SyncTests/Support/MockUser.swift
+++ PocketKit/Tests/SyncTests/Support/MockUser.swift
@@ -1,7 +1,8 @@
 import SharedPocketKit
 
 class MockUser: User {
-    var status: SharedPocketKit.Status?
+    var status: SharedPocketKit.Status = .free
+
     private var implementations: [String: Any] = [:]
     private var calls: [String: [Any]] = [:]
 }
