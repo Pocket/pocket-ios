4ce81bdb72b60da90683409c06c7f6ebd1664d7d
Gio <g.ruscigno@icloud.com>
2023-02-14 12:37:33 -0600

    feat(premium): add PremiumSubscription, PremiumSubscriptionStore types

---
+++ PocketKit/Sources/PocketKit/Premium/Model/PremiumSubscription.swift
@@ -0,0 +1,59 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import StoreKit
+
+/// An enum that maps all available subscription IDs on the App Store
+enum PremiumSubscriptionType: CaseIterable {
+    case monthly
+    case annual
+
+    var id: String {
+        switch self {
+        case .monthly:
+            return Keys.shared.pocketPremiumMonthlyAlpha
+        case .annual:
+            return Keys.shared.pocketPremiumAnnualAlpha
+        }
+    }
+
+    static func type(from productId: String) -> Self? {
+        switch productId {
+        case Keys.shared.pocketPremiumMonthlyAlpha:
+            return .monthly
+        case Keys.shared.pocketPremiumAnnualAlpha:
+            return .annual
+        default:
+            return .none
+        }
+    }
+}
+
+/// A type that maps to a subscription product on the App Store
+struct PremiumSubscription {
+    let product: Product
+    var isPurchased = false
+
+    var name: String {
+        product.displayName
+    }
+
+    var priceDescription: String {
+        product.displayPrice + product.description
+    }
+
+    var type: PremiumSubscriptionType? {
+        PremiumSubscriptionType.type(from: product.id)
+    }
+}
+
+extension Array where Element == PremiumSubscription {
+    init() async throws {
+        self = try await Product
+            .products(for: PremiumSubscriptionType.allCases.map { $0.id })
+            .map {
+                PremiumSubscription(product: $0)
+            }
+    }
+}
