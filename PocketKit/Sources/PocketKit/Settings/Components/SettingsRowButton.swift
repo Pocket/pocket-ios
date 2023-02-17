a635f8a0424d9925380536fe9872dbed4b4f2bed
Gio <g.ruscigno@icloud.com>
2023-02-17 15:08:44 -0600

    fix(premium): fix image color in settings go premium row

--- PocketKit/Sources/PocketKit/Settings/Components/SettingsRowButton.swift
+++ PocketKit/Sources/PocketKit/Settings/Components/SettingsRowButton.swift
@@ -5,8 +5,9 @@ struct SettingsRowButton: View {
     var title: String
     var titleStyle: Style = .settings.row.default
     var icon: SFIconModel?
-    var leadingImage: UIImage?
-    var trailingImage: UIImage?
+    var leadingImageAsset: ImageAsset?
+    var trailingImageAsset: ImageAsset?
+    var tintColor: Color?
 
     let action: () -> Void
 
@@ -15,8 +16,8 @@ struct SettingsRowButton: View {
             self.action()
         } label: {
             HStack(spacing: 0) {
-                if let leadingImage = leadingImage {
-                    Image(uiImage: leadingImage)
+                if let leadingImageAsset, let tintColor {
+                    SettingsButtonImage(color: tintColor, asset: leadingImageAsset)
                         .padding(.trailing)
                 }
                 Text(title)
@@ -26,11 +27,22 @@ struct SettingsRowButton: View {
                 if let icon = icon {
                     SFIcon(icon)
                 }
-                if let trailingImage = trailingImage {
-                    Image(uiImage: trailingImage)
+                if let trailingImageAsset, let tintColor {
+                    SettingsButtonImage(color: tintColor, asset: trailingImageAsset)
                 }
             }
             .padding(.vertical, 5)
         }
+    }
+}
+
+struct SettingsButtonImage: View {
+    let color: Color
+    let asset: ImageAsset
+
+    var body: some View {
+        Image(uiImage: UIImage(asset: asset))
+            .renderingMode(.template)
+            .foregroundColor(color)
     }
 }
