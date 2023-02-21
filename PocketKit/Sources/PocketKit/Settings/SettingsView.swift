b32d7932222f441bbbca5bde16c80b49d79fd00e
timc-mozilla <tchoh@mozilla.com>
2023-02-21 14:44:32 -0600

    settings conflicts

--- PocketKit/Sources/PocketKit/Settings/SettingsView.swift
+++ PocketKit/Sources/PocketKit/Settings/SettingsView.swift
@@ -42,50 +42,11 @@ struct SettingsForm: View {
     @ObservedObject
     var model: AccountViewModel
 
-    @ViewBuilder
-    func topSection() -> some View {
-            Section(header: Text(L10n.yourAccount).style(.settings.header)) {
-                if !model.isPremium {
-                    SettingsRowButton(title: "Go Premium", leadingImage: UIImage(asset: .premiumIcon), trailingImage: UIImage(asset: .chevronRight)) {
-                        model.showPremiumUpgrade()
-                    }
-                    .sheet(isPresented: $model.isPresentingPremiumUpgrade) {
-                        PremiumUpgradeView(viewModel: model.makePremiumUpgradeViewModel())
-                    }
-                }
-                SettingsRowButton(title: L10n.signOut, titleStyle: .settings.button.signOut, icon: SFIconModel("rectangle.portrait.and.arrow.right", weight: .semibold, color: Color(.ui.apricot1))) { model.isPresentingSignOutConfirm.toggle() }
-                    .accessibilityIdentifier("sign-out-button")
-                    .alert(
-                        L10n.areYouSure,
-                        isPresented: $model.isPresentingSignOutConfirm,
-                        actions: {
-                            Button(L10n.signOut, role: .destructive) {
-                                model.signOut()
-                            }
-                        }, message: {
-                            Text(L10n.youWillBeSignedOutOfYourAccountAndAnyFilesThatHaveBeenSavedForOfflineViewingWillBeDeleted)
-                        }
-                    )
-            }
-    }
-
-    @ViewBuilder
-    func topSectionWithLeadingDivider() -> some View {
-        if #available(iOS 16.0, *) {
-            topSection()
-                .alignmentGuide(.listRowSeparatorLeading) { _ in
-                    return 0
-                }
-        } else {
-            topSection()
-        }
-    }
-
     var body: some View {
         Form {
             Group {
                 topSectionWithLeadingDivider()
-                .textCase(nil)
+                    .textCase(nil)
 
                 Section(header: Text(L10n.appCustomization).style(.settings.header)) {
                     SettingsRowToggle(title: L10n.showAppBadgeCount, model: model) {
@@ -132,6 +93,63 @@ struct SettingsForm: View {
                 }
                 Spacer()
             }
+        }
+    }
+}
+
+// MARK: Top Section
+// These methods should be removed once we support iOS 16+
+extension SettingsForm {
+    @ViewBuilder
+    /// Handles top section separator on different versions of iOS
+    private func topSectionWithLeadingDivider() -> some View {
+        if #available(iOS 16.0, *) {
+            topSection()
+                .alignmentGuide(.listRowSeparatorLeading) { _ in
+                    return 0
+                }
+        } else {
+            topSection()
+        }
+    }
+
+    /// Provides the standard top section view
+    private func topSection() -> some View {
+        Section(header: Text(L10n.yourAccount).style(.settings.header)) {
+            if !model.isPremium {
+                SettingsRowButton(
+                    title: L10n.Settings.goPremium,
+                    leadingImageAsset: .premiumIcon,
+                    trailingImageAsset: .chevronRight,
+                    tintColor: Color(.ui.black1)
+                ) {
+                    model.showPremiumUpgrade()
+                }
+                .sheet(isPresented: $model.isPresentingPremiumUpgrade) {
+                    PremiumUpgradeView(viewModel: model.makePremiumUpgradeViewModel())
+                }
+            }
+            SettingsRowButton(
+                title: L10n.signOut,
+                titleStyle: .settings.button.signOut,
+                icon: SFIconModel(
+                    "rectangle.portrait.and.arrow.right",
+                    weight: .semibold,
+                    color: Color(.ui.apricot1)
+                )
+            ) { model.isPresentingSignOutConfirm.toggle() }
+                .accessibilityIdentifier("sign-out-button")
+                .alert(
+                    L10n.areYouSure,
+                    isPresented: $model.isPresentingSignOutConfirm,
+                    actions: {
+                        Button(L10n.signOut, role: .destructive) {
+                            model.signOut()
+                        }
+                    }, message: {
+                        Text(L10n.youWillBeSignedOutOfYourAccountAndAnyFilesThatHaveBeenSavedForOfflineViewingWillBeDeleted)
+                    }
+                )
         }
     }
 }
