179d702105c959ceb0a930536f84c9ef3d611228
Gio <g.ruscigno@icloud.com>
2023-02-22 13:36:37 -0600

    feat(premium): update User, refactor protocol and concrete class to publish status changes

--- PocketKit/Sources/SharedPocketKit/User.swift
+++ PocketKit/Sources/SharedPocketKit/User.swift
@@ -9,25 +9,43 @@ public enum Status: String {
 
 public protocol User {
     var status: Status { get }
+    var statusPublisher: Published<Status>.Publisher { get }
+    var publishedStatus: Published<Status> { get }
     func setPremiumStatus(_ isPremium: Bool)
     func clear()
 }
 
-public class PocketUser: User {
-    static let userStatusKey = "User.statusKey"
+public class PocketUser: User, ObservableObject {
+    @Published public private(set) var status: Status = .unknown
+    public var statusPublisher: Published<Status>.Publisher { $status }
+    public var publishedStatus: Published<Status> { _status }
+    @AppStorage private var storedStatus: Status
 
-    @AppStorage
-    public var status: Status
+    private static let userStatusKey = "User.statusKey"
 
     public init(status: Status = .unknown, userDefaults: UserDefaults) {
-        _status = AppStorage(wrappedValue: status, Self.userStatusKey, store: userDefaults)
+        _storedStatus = AppStorage(wrappedValue: status, Self.userStatusKey, store: userDefaults)
+        publishStatus()
     }
 
     public func setPremiumStatus(_ isPremium: Bool) {
-        status = isPremium ? .premium : .free
+        let targetStatus: Status = isPremium ? .premium : .free
+        setStatus(targetStatus)
     }
 
     public func clear() {
-        status = .unknown
+        setStatus(.unknown)
+    }
+}
+
+// MARK: Private helpers
+extension PocketUser {
+    private func setStatus(_ status: Status) {
+        storedStatus = status
+        publishStatus()
+    }
+
+    private func publishStatus() {
+        status = storedStatus
     }
 }
