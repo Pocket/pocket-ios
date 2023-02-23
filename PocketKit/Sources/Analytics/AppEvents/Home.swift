de0b7177e474325217bf9e4b90033eb74172be88
Daniel Brooks <bass_rock@me.com>
2023-02-23 11:42:05 -0800

    fix(urls): not force unwrapping urls

--- PocketKit/Sources/Analytics/AppEvents/Home.swift
+++ PocketKit/Sources/Analytics/AppEvents/Home.swift
@@ -11,7 +11,7 @@ public extension Events.Home {
     /**
      Fired when a card in the `Recent Saves` section scrolls into view
      */
-    static func RecentSavesCardImpression(url: URL!, positionInList: Int) -> Impression {
+    static func RecentSavesCardImpression(url: URL, positionInList: Int) -> Impression {
         return Impression(
             component: .card,
             requirement: .viewable,
@@ -29,7 +29,7 @@ public extension Events.Home {
     /**
      Fired when a card in the `Recent Saves` section is shared
      */
-    static func RecentSavesCardShare(url: URL!, positionInList: Int) -> Engagement {
+    static func RecentSavesCardShare(url: URL, positionInList: Int) -> Engagement {
         return Engagement(
             uiEntity: UiEntity(
                 .button,
@@ -45,7 +45,7 @@ public extension Events.Home {
     /**
      Fired when a card in the `Recent Saves` section is deleted
      */
-    static func RecentSavesCardDelete(url: URL!, positionInList: Int) -> Engagement {
+    static func RecentSavesCardDelete(url: URL, positionInList: Int) -> Engagement {
         return Engagement(
             uiEntity: UiEntity(
                 .button,
@@ -61,7 +61,7 @@ public extension Events.Home {
     /**
      Fired when a card in the `Recent Saves` section is archived
      */
-    static func RecentSavesCardArchive(url: URL!, positionInList: Int) -> Engagement {
+    static func RecentSavesCardArchive(url: URL, positionInList: Int) -> Engagement {
         return Engagement(
             uiEntity: UiEntity(
                 .button,
@@ -77,7 +77,7 @@ public extension Events.Home {
     /**
      Fired when a user clicks a card in the `Recent Saves` section
      */
-    static func RecentSavesCardContentOpen(url: URL!, positionInList: Int) -> ContentOpen {
+    static func RecentSavesCardContentOpen(url: URL, positionInList: Int) -> ContentOpen {
         return ContentOpen(
             contentEntity:
                 ContentEntity(url: url),
@@ -92,7 +92,7 @@ public extension Events.Home {
     /**
      Fired when a user clicks a card on Home using the /discover API
      */
-    static func SlateArticleContentOpen(url: URL!, positionInList: Int, slateId: String, slateRequestId: String, slateExperimentId: String, slateIndex: Int, slateLineupId: String, slateLineupRequestId: String, slateLineupExperimentId: String, recommendationId: String) -> ContentOpen {
+    static func SlateArticleContentOpen(url: URL, positionInList: Int, slateId: String, slateRequestId: String, slateExperimentId: String, slateIndex: Int, slateLineupId: String, slateLineupRequestId: String, slateLineupExperimentId: String, recommendationId: String) -> ContentOpen {
         return ContentOpen(
             contentEntity:
                 ContentEntity(url: url),
@@ -112,7 +112,7 @@ public extension Events.Home {
     /**
      Fired when a user sees a card on Home using the /discover API
      */
-    static func SlateArticleImpression(url: URL!, positionInList: Int, slateId: String, slateRequestId: String, slateExperimentId: String, slateIndex: Int, slateLineupId: String, slateLineupRequestId: String, slateLineupExperimentId: String, recommendationId: String) -> Impression {
+    static func SlateArticleImpression(url: URL, positionInList: Int, slateId: String, slateRequestId: String, slateExperimentId: String, slateIndex: Int, slateLineupId: String, slateLineupRequestId: String, slateLineupExperimentId: String, recommendationId: String) -> Impression {
         return Impression(
             component: .card,
             requirement: .viewable,
@@ -133,7 +133,7 @@ public extension Events.Home {
     /**
      Fired when a user saves a card on Home using the /discover API
      */
-    static func SlateArticleSave(url: URL!, positionInList: Int, slateId: String, slateRequestId: String, slateExperimentId: String, slateIndex: Int, slateLineupId: String, slateLineupRequestId: String, slateLineupExperimentId: String, recommendationId: String) -> Engagement {
+    static func SlateArticleSave(url: URL, positionInList: Int, slateId: String, slateRequestId: String, slateExperimentId: String, slateIndex: Int, slateLineupId: String, slateLineupRequestId: String, slateLineupExperimentId: String, recommendationId: String) -> Engagement {
         return Engagement(
             .save(
                 contentEntity: ContentEntity(url: url)
@@ -154,7 +154,7 @@ public extension Events.Home {
     /**
      Fired when a user unsaves a card on Home using the /discover API
      */
-    static func SlateArticleUnsave(url: URL!, positionInList: Int, slateId: String, slateRequestId: String, slateExperimentId: String, slateIndex: Int, slateLineupId: String, slateLineupRequestId: String, slateLineupExperimentId: String, recommendationId: String) -> Engagement {
+    static func SlateArticleUnsave(url: URL, positionInList: Int, slateId: String, slateRequestId: String, slateExperimentId: String, slateIndex: Int, slateLineupId: String, slateLineupRequestId: String, slateLineupExperimentId: String, recommendationId: String) -> Engagement {
         return Engagement(
             .general,
             uiEntity: UiEntity(
@@ -174,7 +174,7 @@ public extension Events.Home {
     /**
      Fired when a user archives a card on Home using the /discover API
      */
-    static func SlateArticleArchive(url: URL!, positionInList: Int, slateId: String, slateRequestId: String, slateExperimentId: String, slateIndex: Int, slateLineupId: String, slateLineupRequestId: String, slateLineupExperimentId: String, recommendationId: String) -> Engagement {
+    static func SlateArticleArchive(url: URL, positionInList: Int, slateId: String, slateRequestId: String, slateExperimentId: String, slateIndex: Int, slateLineupId: String, slateLineupRequestId: String, slateLineupExperimentId: String, recommendationId: String) -> Engagement {
         return Engagement(
             .general,
             uiEntity: UiEntity(
@@ -194,7 +194,7 @@ public extension Events.Home {
     /**
      Fired when a user shares a card on Home using the /discover API
      */
-    static func SlateArticleShare(url: URL!, positionInList: Int, slateId: String, slateRequestId: String, slateExperimentId: String, slateIndex: Int, slateLineupId: String, slateLineupRequestId: String, slateLineupExperimentId: String, recommendationId: String) -> Engagement {
+    static func SlateArticleShare(url: URL, positionInList: Int, slateId: String, slateRequestId: String, slateExperimentId: String, slateIndex: Int, slateLineupId: String, slateLineupRequestId: String, slateLineupExperimentId: String, recommendationId: String) -> Engagement {
         return Engagement(
             .general,
             uiEntity: UiEntity(
@@ -214,7 +214,7 @@ public extension Events.Home {
     /**
      Fired when a user selects the report action on Home using the /discover API
      */
-    static func SlateArticleReport(url: URL!, reason: ReportEntity.Reason, comment: String?) -> Engagement {
+    static func SlateArticleReport(url: URL, reason: ReportEntity.Reason, comment: String?) -> Engagement {
         return Engagement(
             .report(
                 reportEntity: ReportEntity(reason: reason, comment: comment),
