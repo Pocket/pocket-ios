6ef934fb17dde9aaa13e680e36c5094f6ade1282
Gio <g.ruscigno@icloud.com>
2023-03-07 16:22:04 -0600

    feat(premium): update PremiumTests, some minor refactor

--- Tests iOS/PremiumTests.swift
+++ Tests iOS/PremiumTests.swift
@@ -14,9 +14,10 @@ class PremiumTests: XCTestCase {
 
     override func setUp() async throws {
         continueAfterFailure = false
-        storeSession = try! SKTestSession(configurationFileNamed: "Test_Subscriptions")
-        storeSession.disableDialogs = true
+        storeSession = try SKTestSession(configurationFileNamed: "Test_Subscriptions")
+        storeSession.resetToDefaultState()
         storeSession.clearTransactions()
+        storeSession.disableDialogs = true
         app = PocketAppElement(app: XCUIApplication())
         server = Application()
 
@@ -27,19 +28,22 @@ class PremiumTests: XCTestCase {
     override func tearDownWithError() throws {
         try server.stop()
         storeSession.clearTransactions()
+        storeSession.resetToDefaultState()
         storeSession = nil
         app.terminate()
     }
 
-    @MainActor
-    func test_tapGoPremiumShowsUpgradeView() async {
+
+    /// Test that tapping "Go Premium" in Settings presents the Premium Upgrade view
+    @MainActor func test_tapGoPremiumShowsUpgradeView() async {
         configureFreeUser()
         app.launch()
         await loadPremiumUpgradeView()
     }
 
-    @MainActor
-    func test_tapDismiss_dismissPremiumView() async {
+
+    /// Test that Premium Upgrade view dismisses when tapping the dismiss button
+    @MainActor func test_tapDismissDismissPremiumView() async {
         // Given
         configureFreeUser()
         app.launch()
@@ -52,8 +56,9 @@ class PremiumTests: XCTestCase {
         XCTAssertFalse(app.premiumUpgradeView.exists)
     }
 
-    @MainActor
-    func test_monthlyButtonTapped() async {
+
+    /// Test that monthly button tapped triggers the right event
+    @MainActor func test_monthlyButtonTapped() async {
         // Given
         configureFreeUser()
         app.launch()
@@ -65,8 +70,9 @@ class PremiumTests: XCTestCase {
         XCTAssertNotNil(monthlyButtonTappedEvent)
     }
 
-    @MainActor
-    func test_annualButtonTapped() async {
+
+    /// Test that annual button tapped triggers the right event
+    @MainActor func test_annualButtonTapped() async {
         // Given
         configureFreeUser()
         app.launch()
@@ -78,8 +84,9 @@ class PremiumTests: XCTestCase {
         XCTAssertNotNil(monthlyButtonTappedEvent)
     }
 
-    @MainActor
-    func test_purchaseMonthlySubscriptionSuccess() async {
+
+    /// Test that purchase monthly subscription succeeds
+    @MainActor func test_purchaseMonthlySubscriptionSuccess() async {
         // Given
         configureFreeUser()
         app.launch()
@@ -91,8 +98,8 @@ class PremiumTests: XCTestCase {
         XCTAssertNotNil(purchaseSuccessEvent)
     }
 
-    @MainActor
-    func test_purchaseAnnualSubscriptionSuccess() async {
+    /// Test that purchase annual subscription succeeds
+    @MainActor func test_purchaseAnnualSubscriptionSuccess() async {
         // Given
         configureFreeUser()
         app.launch()
@@ -104,8 +111,7 @@ class PremiumTests: XCTestCase {
         XCTAssertNotNil(purchaseSuccessEvent)
     }
 
-    @MainActor
-    func configureFreeUser() {
+    @MainActor func configureFreeUser() {
         server.routes.post("/graphql") { request, _ in
             let apiRequest = ClientAPIRequest(request)
             if apiRequest.isForSavesContent {
@@ -115,8 +121,7 @@ class PremiumTests: XCTestCase {
         }
     }
 
-    @MainActor
-    func loadPremiumUpgradeView() async {
+    @MainActor func loadPremiumUpgradeView() async {
         app.tabBar.settingsButton.wait().tap()
         XCTAssertTrue(app.settingsView.exists)
 
