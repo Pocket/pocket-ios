393d94d55a568db2d0664ac45122d310af9f5735
Gio <g.ruscigno@icloud.com>
2023-03-14 15:26:38 -0500

    feat(premium): update ReceiptService, minor refactor

--- PocketKit/Sources/PocketKit/PremiumUpgrade/Model/ReceiptService.swift
+++ PocketKit/Sources/PocketKit/PremiumUpgrade/Model/ReceiptService.swift
@@ -9,14 +9,13 @@ public protocol ReceiptService {
 
 public enum ReceiptError: Error {
     case noData
+    case invalidReceipt
 }
 
 /// Concrete implementation that sends the App Store Receipt to the Pocket backend
 struct AppStoreReceiptService: ReceiptService {
     func send(_ product: Product?) async throws {
-        let data = try getReceipt()
-        let receiptString = data.base64EncodedString(options: [])
-        let transactionInfo = receiptString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+        let transactionInfo = try getReceipt()
         let source = "itunes"
         let productId = product?.id ?? ""
         let amount = product?.price != nil ? "\(product!.price)" : ""
@@ -25,7 +24,7 @@ struct AppStoreReceiptService: ReceiptService {
 
         try await Services.shared.v3Client.sendAppstoreReceipt(
             source: source,
-            transactionInfo: transactionInfo!,
+            transactionInfo: transactionInfo,
             amount: amount,
             productId: productId,
             currency: currency,
@@ -35,13 +34,20 @@ struct AppStoreReceiptService: ReceiptService {
 }
 
 // MARK: private methods
-extension AppStoreReceiptService {
+private extension AppStoreReceiptService {
     /// Return the App Store receipt if it exists
-    func getReceipt() throws -> Data {
+    func getReceipt() throws -> String {
         guard let appStoreReceiptURL = Bundle.main.appStoreReceiptURL,
               FileManager.default.fileExists(atPath: appStoreReceiptURL.path) else {
             throw ReceiptError.noData
         }
-        return try Data(contentsOf: appStoreReceiptURL, options: .alwaysMapped)
+        let receiptString = try Data(contentsOf: appStoreReceiptURL, options: .alwaysMapped)
+            .base64EncodedString(options: [])
+
+        guard let receipt = receiptString
+            .addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
+            throw ReceiptError.invalidReceipt
+        }
+        return receipt
     }
 }
