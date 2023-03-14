c9f9bd68d8e6dc2f48b25563b444fc1116b1855e
Gio <g.ruscigno@icloud.com>
2023-03-14 14:58:20 -0500

    feat(premium): add ReceiptService, AppStoreReceiptService, a generic service that sends a receipt to a server and a concrete implementation of it for the App Store receipt

--- PocketKit/Sources/PocketKit/PremiumUpgrade/Model/ReceiptService.swift
+++ PocketKit/Sources/PocketKit/PremiumUpgrade/Model/ReceiptService.swift
@@ -1,29 +1,42 @@
 import Foundation
+import StoreKit
 import Sync
 
-/// Generic type to validate a subscription purchase or redeem
+/// Generic type to send subscription receipt to a server
 public protocol ReceiptService {
-    func send()
+    func send(_ product: Product?) async throws
 }
 
 public enum ReceiptError: Error {
     case noData
 }
 
-/// Concrete implementation that sends the App Store Receipt to the backend
+/// Concrete implementation that sends the App Store Receipt to the Pocket backend
 struct AppStoreReceiptService: ReceiptService {
-    func send() {
-        do {
-            let data = try getReceipt()
-            let receiptString = data.base64EncodedString(options: [])
-        } catch {
-            Log.capture(error: error)
-        }
+    func send(_ product: Product?) async throws {
+        let data = try getReceipt()
+        let receiptString = data.base64EncodedString(options: [])
+        let transactionInfo = receiptString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+        let source = "itunes"
+        let productId = product?.id ?? ""
+        let amount = product?.price != nil ? "\(product!.price)" : ""
+        let transactionType = product != nil ? "purchase" : "restore"
+        let currency = product?.priceFormatStyle.currencyCode ?? ""
+
+        try await Services.shared.v3Client.sendAppstoreReceipt(
+            source: source,
+            transactionInfo: transactionInfo!,
+            amount: amount,
+            productId: productId,
+            currency: currency,
+            transactionType: transactionType
+        )
     }
 }
 
 // MARK: private methods
 extension AppStoreReceiptService {
+    /// Return the App Store receipt if it exists
     func getReceipt() throws -> Data {
         guard let appStoreReceiptURL = Bundle.main.appStoreReceiptURL,
               FileManager.default.fileExists(atPath: appStoreReceiptURL.path) else {
