c95d5bbbee98dc472b7831a68e7a18a4a18547b0
Gio <g.ruscigno@icloud.com>
2023-03-27 11:53:38 -0500

    feat(migration): update LegacyCleanupService, add deleteAudioCache()

--- PocketKit/Sources/SharedPocketKit/Migrations/Cleanup/LegacyCleanupService.swift
+++ PocketKit/Sources/SharedPocketKit/Migrations/Cleanup/LegacyCleanupService.swift
@@ -14,6 +14,7 @@ public struct LegacyCleanupService {
         cleanupLegacyUserDefaults()
         deleteSqliteDataBase()
         deleteImageCache()
+        deleteAudioCache()
         deleteLogFiles()
     }
 
