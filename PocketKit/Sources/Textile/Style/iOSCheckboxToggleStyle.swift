e8f08517890d589fc616a16385d73d54c228bfeb
timc-mozilla <tchoh@mozilla.com>
2023-04-05 15:30:54 -0500

    feat(tags): premium upsell

--- PocketKit/Sources/Textile/Style/iOSCheckboxToggleStyle.swift
+++ PocketKit/Sources/Textile/Style/iOSCheckboxToggleStyle.swift
@@ -2,12 +2,9 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-/**
- Inspiration taken from  https://sarunw.com/posts/swiftui-checkbox/
-*/
-
 import SwiftUI
 
+/// Inspiration taken from  https://sarunw.com/posts/swiftui-checkbox/
 public struct iOSCheckboxToggleStyle: ToggleStyle {
     public init() {}
 
--- PocketKit/Sources/Textile/Views/Tags/AddTagsViewModel.swift
+++ PocketKit/Sources/Textile/Views/Tags/AddTagsViewModel.swift
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 import Combine
 import SwiftUI
 
@@ -11,15 +15,16 @@ public protocol AddTagsViewModel: ObservableObject {
     var emptyStateText: String { get }
     var tags: [String] { get set }
     var newTagInput: String { get set }
-    var otherTags: [String] { get set }
+    var otherTags: [TagType] { get set }
     var sectionTitle: TagSectionType { get }
     var upsellView: AnyView { get }
-    
+
     func addNewTag(with tag: String) -> Bool
     func addExistingTag(with tag: TagType)
     func addTags()
     func allOtherTags()
     func removeTag(with tag: String)
+
     func trackAddTag()
     func trackRemoveTag()
 }
@@ -33,24 +38,36 @@ public extension AddTagsViewModel {
         "Organize your items with Tags.\n To create a tag, enter one below."
     }
 
-    /// Add tag after user enters tag name in the text field or taps on a cell in `OtherTagsView`
+    /// Add tag after user enters tag name in the text field
     /// - Parameter tag: tag name user input in the text field
     /// - Returns: true if tag is a valid input
-    func addTag(with tag: String) -> Bool {
+    func addNewTag(with tag: String) -> Bool {
         let tagName = validateInput(tag)
         guard !tagName.isEmpty,
               !tags.contains(tagName) else {
             return false
         }
+        addTag(with: tagName)
+        return true
+    }
+
+    /// Add tag from users list of tags
+    /// - Parameter tag: tag name user tapped on in the list
+    func addExistingTag(with tag: TagType) {
+        addTag(with: tag.name)
+    }
+
+    /// Add tag to the input area and remove from the list
+    /// - Parameter tag: tag name to add in the input area
+    private func addTag(with tagName: String) {
         tags.append(tagName)
-        if let index = otherTags.firstIndex(of: tagName) {
+        if let index = otherTags.firstIndex(where: { $0.name == tagName}) {
             otherTags.remove(at: index)
         }
         if otherTags.isEmpty {
             allOtherTags()
         }
         trackAddTag()
-        return true
     }
 
     /// Removes tag from the `InputTagsView` and adds to list of `OtherTagsView`
@@ -58,7 +75,7 @@ public extension AddTagsViewModel {
     func removeTag(with tag: String) {
         guard let index = tags.firstIndex(of: tag) else { return }
         tags.remove(at: index)
-        otherTags.append(tag)
+        otherTags.append(TagType.tag(tag))
         trackRemoveTag()
     }
 
