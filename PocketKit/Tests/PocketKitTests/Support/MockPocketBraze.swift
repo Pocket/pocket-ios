92ec19bd5530d76212f2e96992b1277593b63536
Conor Masterson <cmasterson@mozilla.com>
2023-04-05 10:23:45 -0400

    Resolve merge conflict

--- PocketKit/Tests/PocketKitTests/Support/MockPocketBraze.swift
+++ PocketKit/Tests/PocketKitTests/Support/MockPocketBraze.swift
@@ -46,11 +46,9 @@ extension MockPocketBraze {
 
         return calls[index] as? DidRecieveUserNotifcationImpl
     }
-<<<<<<< Updated upstream
-=======
 
     func signedInUserDidBeginMigration() {
 
     }
->>>>>>> Stashed changes
+
 }
