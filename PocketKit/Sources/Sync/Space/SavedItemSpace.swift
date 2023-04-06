df19e97c2be5f6370b70f374ceb5ca584d386a71
Gio <g.ruscigno@icloud.com>
2023-04-06 17:38:24 -0500

    refactor(CoreData): add SavedItemSpace, a type that handles bulk page updates on saved and archived items * also created a dedicated folder for Space within Sync

---
+++ PocketKit/Sources/Sync/Space/SavedItemSpace.swift
@@ -0,0 +1,66 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import CoreData
+
+/// A type that handles bulk saving operations on saved or archived items
+struct SavedItemSpace {
+    private let space: Space
+    private let context: NSManagedObjectContext
+    private let task: PersistentSyncTask
+
+    var currentCursor: String? {
+        task.currentCursor
+    }
+
+    init?(space: Space, taskID: NSManagedObjectID) {
+        let context = space.makeChildBackgroundContext()
+
+        var fetchedTask: PersistentSyncTask?
+        context.performAndWait {
+            fetchedTask = context.object(with: taskID) as? PersistentSyncTask
+
+        }
+        guard let task = fetchedTask else {
+            return nil
+        }
+        self.space = space
+        self.context = context
+        self.task = task
+    }
+
+    func savePage(edges: [SavedItem.SavedItemEdge?], cursor: String) throws {
+        for edge in edges {
+            guard let edge = edge, let node = edge.node, let url = URL(string: node.url) else {
+                return
+            }
+
+            Log.breadcrumb(
+                category: "sync",
+                level: .info,
+                message: "Updating/Inserting SavedItem with ID: \(node.remoteID)"
+            )
+
+            context.performAndWait {
+                let item = (try? space.fetchSavedItem(byRemoteID: node.remoteID, context: context)) ?? SavedItem(context: context, url: url, remoteID: node.remoteID)
+                item.update(from: edge, with: space)
+
+                if item.deletedAt != nil {
+                    space.delete(item)
+                }
+                task.currentCursor = cursor
+            }
+
+            // save the child context
+            try context.performAndWait {
+                guard context.hasChanges else {
+                    return
+                }
+                try context.save()
+                // then save the parent context
+                try space.save()
+            }
+        }
+    }
+}
