c3deaec8bc6e55ca26bcc77ef3785ba61afbe564
Daniel Brooks <bass_rock@me.com>
2023-04-08 14:26:38 -0700

    feat(spotlight): adding in spotlight indexing.

---
+++ PocketKit/Sources/Sync/CoreDataSpotlightDelegate.swift
@@ -0,0 +1,47 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import Foundation
+import CoreData
+import CoreSpotlight
+
+/// Delegate to listen to callbacks from CoreData when something needs to be indexed
+class CoreDataSpotlightDelegate: NSCoreDataCoreSpotlightDelegate {
+    override func domainIdentifier() -> String {
+        return "com.mozilla.pocket"
+    }
+
+    override func indexName() -> String? {
+        return "pocket-index"
+    }
+
+    override func attributeSet(for object: NSManagedObject) -> CSSearchableItemAttributeSet? {
+        if let savedItem = object as? SavedItem {
+            return csSearchableItemAttributeSet(for: savedItem)
+        }
+
+        return nil
+    }
+    
+    /// Helper function to turn a SavedItem into a CSSearchableItemAttributeSet
+    /// - Parameter savedItem: The saved item to search
+    /// - Returns: The CoreSpotlight result
+    private func csSearchableItemAttributeSet(for savedItem: SavedItem) -> CSSearchableItemAttributeSet? {
+        guard let identifier = savedItem.remoteID else {
+            return nil
+        }
+        let attributeSet = CSSearchableItemAttributeSet(contentType: .content)
+        attributeSet.identifier = identifier
+        attributeSet.displayName = savedItem.item?.title
+        attributeSet.publishers = (savedItem.item?.authors?.array as? [Author] ?? []).compactMap { $0.name }
+        attributeSet.thumbnailURL = savedItem.item?.topImageURL // TODO: Image cache url..
+        attributeSet.contentURL = savedItem.url
+        attributeSet.contentDescription = savedItem.item?.excerpt
+        attributeSet.title = savedItem.item?.title
+        attributeSet.contentCreationDate = savedItem.item?.datePublished
+
+        return attributeSet
+    }
+
+}
