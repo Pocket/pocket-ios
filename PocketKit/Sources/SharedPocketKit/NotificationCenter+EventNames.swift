b8324dd0037af97dd256b4703c2ff0453db97987
Daniel Brooks <bass_rock@me.com>
2023-04-11 17:13:25 -0700

    fix(migration): ensure that a migration posts a logged in message (#592)

---
+++ PocketKit/Sources/SharedPocketKit/NotificationCenter+EventNames.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+public extension Notification.Name {
+    public static let userLoggedIn = Notification.Name("com.mozilla.pocket.userLoggedIn")
+    public static let userLoggedOut = Notification.Name("com.mozilla.pocket.userLoggedOut")
+    public static let listUpdated = Notification.Name("com.mozilla.pocket.listUpdated")
+}
