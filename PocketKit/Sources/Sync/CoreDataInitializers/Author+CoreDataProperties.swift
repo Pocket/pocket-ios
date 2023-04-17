7df3bf9d751b0bec690481667530fd45fb516e30
Gio <gruscigno@mozilla.com>
2023-04-17 14:36:11 -0500

    refactor(Core Data): add manual subclass for Author

---
+++ PocketKit/Sources/Sync/CoreDataInitializers/Author+CoreDataProperties.swift
@@ -0,0 +1,18 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+
+import Foundation
+import CoreData
+
+extension Author {
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Author> {
+        return NSFetchRequest<Author>(entityName: "Author")
+    }
+
+    @NSManaged public var id: String
+    @NSManaged public var name: String?
+    @NSManaged public var url: URL?
+    @NSManaged public var item: Item?
+}
