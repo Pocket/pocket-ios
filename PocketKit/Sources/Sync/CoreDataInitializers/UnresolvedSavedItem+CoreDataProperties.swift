a496f21f13793d5a7ca7b39eb0b39d8635a65af2
Gio <gruscigno@mozilla.com>
2023-04-17 14:54:00 -0500

    refactor(Core Data): add manual subclass for FeatureFlag, PeristentSyncTask, SavedItemUpdatedNotification, UnresolvedSavedItem

---
+++ PocketKit/Sources/Sync/CoreDataInitializers/UnresolvedSavedItem+CoreDataProperties.swift
@@ -0,0 +1,15 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+
+import Foundation
+import CoreData
+
+extension UnresolvedSavedItem {
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<UnresolvedSavedItem> {
+        return NSFetchRequest<UnresolvedSavedItem>(entityName: "UnresolvedSavedItem")
+    }
+
+    @NSManaged public var savedItem: SavedItem?
+}
