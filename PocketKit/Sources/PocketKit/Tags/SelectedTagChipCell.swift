9dd9903335015b67958e60f345b7ab09ebaa1175
timc-mozilla <tchoh@mozilla.com>
2023-04-18 09:09:06 -0500

    fix(filters): tag filter 'x' spacing

--- PocketKit/Sources/PocketKit/Tags/SelectedTagChipCell.swift
+++ PocketKit/Sources/PocketKit/Tags/SelectedTagChipCell.swift
@@ -133,17 +133,3 @@ extension SelectedTagChipCell {
 private extension Style {
     static let title: Style = .header.sansSerif.h8.with(color: .ui.teal1)
 }
-
-extension UIImage {
-    func addImagePadding(width: CGFloat, height: CGFloat) -> UIImage? {
-        let maxWidth: CGFloat = size.width + width
-        let maxHeight: CGFloat = size.height + height
-        UIGraphicsBeginImageContextWithOptions(CGSize(width: maxWidth, height: maxHeight), false, 0)
-        let origin: CGPoint = CGPoint(x: (maxWidth - size.width) / 2, y: (maxHeight - size.height) / 2)
-        draw(at: origin)
-        let imageWithPadding = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-
-        return imageWithPadding
-    }
-}
