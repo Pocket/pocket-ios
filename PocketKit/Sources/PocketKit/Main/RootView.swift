b9c0539b8e30a8953fb4b84ca73b9907217d07ab
Daniel Brooks <bass_rock@me.com>
2023-04-24 18:13:23 -0700

    fix(tabBar): brining back the tab bar appearance

--- PocketKit/Sources/PocketKit/Main/RootView.swift
+++ PocketKit/Sources/PocketKit/Main/RootView.swift
@@ -5,6 +5,36 @@ public struct RootView: View {
 
     public init(model: RootViewModel) {
         self.model = model
+        self.setupTabBarAppearance()
+    }
+
+    /// Sets up the tab bar appearance following some of https://stackoverflow.com/questions/56969309/change-tabbed-view-bar-color-swiftui
+    private func setupTabBarAppearance() {
+        let tabBarAppeareance = UITabBarAppearance()
+        tabBarAppeareance.configureWithOpaqueBackground()
+
+        tabBarAppeareance.shadowColor = UIColor(.ui.grey1) // For line separator of the tab bar
+        tabBarAppeareance.backgroundColor = UIColor(.ui.white1)
+        tabBarAppeareance.backgroundEffect = nil
+        tabBarAppeareance.selectionIndicatorTintColor = UIColor(.ui.grey1)
+
+        tabBarAppeareance.stackedLayoutAppearance = tabItemAppearance
+        tabBarAppeareance.compactInlineLayoutAppearance = tabItemAppearance
+        tabBarAppeareance.inlineLayoutAppearance = tabItemAppearance
+
+        // Use this appearance when scrolling behind the TabView:
+        UITabBar.appearance().standardAppearance = tabBarAppeareance
+        // Use this appearance when scrolled all the way up:
+        UITabBar.appearance().scrollEdgeAppearance = tabBarAppeareance
+    }
+
+    private var tabItemAppearance: UITabBarItemAppearance {
+       let tabItemAppeareance =  UITabBarItemAppearance()
+       tabItemAppeareance.selected.iconColor = UIColor(.ui.grey1)
+       tabItemAppeareance.selected.titleTextAttributes[.foregroundColor] = UIColor(.ui.grey1)
+       tabItemAppeareance.normal.iconColor = UIColor(.ui.grey1)
+       tabItemAppeareance.normal.titleTextAttributes[.foregroundColor] = UIColor(.ui.grey1)
+       return tabItemAppeareance
     }
 
     public var body: some View {
