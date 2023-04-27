51814809300570fd86cd6db6a6f13cd5546c4b73
Gio <gruscigno@mozilla.com>
2023-04-27 14:02:31 -0500

    refactor(Recommendation): update Requests, do not fetch orphaned recommendations (recommendations with no item)

--- PocketKit/Sources/Sync/Requests.swift
+++ PocketKit/Sources/Sync/Requests.swift
@@ -89,7 +89,7 @@ public enum Requests {
 
     public static func fetchRecomendations(by lineupIdentifier: String) -> RichFetchRequest<Recommendation> {
         let request = RichFetchRequest<Recommendation>(entityName: "Recommendation")
-        request.predicate = NSPredicate(format: "slate.slateLineup.remoteID = %@", lineupIdentifier)
+        request.predicate = NSPredicate(format: "slate.slateLineup.remoteID = %@ AND item != NULL", lineupIdentifier)
         request.sortDescriptors = [
             NSSortDescriptor(keyPath: \Recommendation.slate?.sortIndex, ascending: true),
             NSSortDescriptor(keyPath: \Recommendation.sortIndex, ascending: true),
