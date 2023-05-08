034c8ea6a817de27f85863d68b8d6eb3fcff61b2
Daniel Brooks <bass_rock@me.com>
2023-05-08 16:30:56 -0700

    fix(retry): disabling retry on a 500

--- PocketKit/Sources/Sync/Operations/FetchArchive.swift
+++ PocketKit/Sources/Sync/Operations/FetchArchive.swift
@@ -75,7 +75,7 @@ class FetchArchive: SyncOperation {
                         level: .error,
                         message: "ResponseCodeInterceptor.ResponseCodeError with Error: \(error.localizedDescription) and status code \(String(describing: response?.statusCode))"
                     )
-                    return .retry(error)
+                    return .failure(error)
                 default:
                     return .failure(error)
                 }
