1febc4ee282a5840646e45372d535a25cc31ea5c
Gio <gruscigno@mozilla.com>
2023-05-22 10:58:52 -0500

    feat(widgets): update RecentSavesWidget, use the newly created RecentSavesView

--- PocketKit/Sources/RecentSavesWidgetKit/RecentSavesWidget.swift
+++ PocketKit/Sources/RecentSavesWidgetKit/RecentSavesWidget.swift
@@ -5,42 +5,8 @@
 import WidgetKit
 import SwiftUI
 
-struct Provider: TimelineProvider {
-    func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date())
-    }
-
-    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> Void) {
-        let entry = SimpleEntry(date: Date())
-        completion(entry)
-    }
-
-    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> Void) {
-        var entries: [SimpleEntry] = []
-
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate)
-            entries.append(entry)
-        }
-
-        let timeline = Timeline(entries: entries, policy: .atEnd)
-        completion(timeline)
-    }
-}
-
 struct SimpleEntry: TimelineEntry {
     let date: Date
-}
-
-struct RecentSavesWidgetEntryView: View {
-    var entry: Provider.Entry
-
-    var body: some View {
-        Text(entry.date, style: .time)
-    }
 }
 
 public struct RecentSavesWidget: Widget {
@@ -51,8 +17,8 @@ public struct RecentSavesWidget: Widget {
     }
 
     public var body: some WidgetConfiguration {
-        StaticConfiguration(kind: kind, provider: Provider()) { entry in
-            RecentSavesWidgetEntryView(entry: entry)
+        StaticConfiguration(kind: kind, provider: RecentSavesProvider()) { entry in
+            RecentSavesView()
         }
         .configurationDisplayName("My Widget")
         .description("This is an example widget.")
@@ -61,7 +27,7 @@ public struct RecentSavesWidget: Widget {
 
 public struct RecentSavesWidget_Previews: PreviewProvider {
     public static var previews: some View {
-        RecentSavesWidgetEntryView(entry: SimpleEntry(date: Date()))
+        RecentSavesView()
             .previewContext(WidgetPreviewContext(family: .systemSmall))
     }
 }
