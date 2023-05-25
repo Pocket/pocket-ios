7812181ac58aa5bbb57516c16d0040f0f36641fe
cyndichin <cgc95@cornell.edu>
2023-05-25 16:24:17 -0400

    ci(swiftlint): address file header errors

--- PocketKit/Package.swift
+++ PocketKit/Package.swift
@@ -1,6 +1,10 @@
 // swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 import PackageDescription
 
 let package = Package(
