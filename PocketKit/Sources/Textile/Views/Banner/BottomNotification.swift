5e1b5a63b006e44c45a7ce0b2ea2c2373a9ca51e
cyndichin <cgc95@cornell.edu>
2023-06-14 09:38:19 -0400

    feat(reportIssue): remove unnecessary import and change reportButton signature

--- PocketKit/Sources/Textile/Views/Banner/BottomNotification.swift
+++ PocketKit/Sources/Textile/Views/Banner/BottomNotification.swift
@@ -3,7 +3,6 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import SwiftUI
-import UIKit
 
 // Added for code convenience
 public typealias BannerAction = BannerModifier.BannerData.BannerAction
