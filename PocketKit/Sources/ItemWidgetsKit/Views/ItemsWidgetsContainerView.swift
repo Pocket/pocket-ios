f492c70f0006b776da8f7768e68dbce0427f3df7
Gio <gruscigno@mozilla.com>
2023-07-10 12:45:55 -0500

    feat(widgets): add iOS 17 specific code to handle background

--- PocketKit/Sources/ItemWidgetsKit/Views/ItemsWidgetsContainerView.swift
+++ PocketKit/Sources/ItemWidgetsKit/Views/ItemsWidgetsContainerView.swift
@@ -33,10 +33,18 @@ struct ItemsWidgetsContainerView: View {
     }
 
     var body: some View {
-        makeContainerView()
-            .padding()
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(Color(.ui.white1))
+        if #available(iOS 17.0, *) {
+            makeContainerView()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .containerBackground(for: .widget) {
+                    Color(.ui.white1)
+                }
+        } else {
+            makeContainerView()
+                .padding()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Color(.ui.white1))
+        }
     }
 
     @ViewBuilder
