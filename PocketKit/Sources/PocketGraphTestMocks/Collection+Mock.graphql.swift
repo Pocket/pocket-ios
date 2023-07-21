5f5bc7de144f165f372d4877b4a425168dd986b2
cyndichin <cgc95@cornell.edu>
2023-07-21 14:45:54 -0400

    feat(collections): add core data models

--- PocketKit/Sources/PocketGraphTestMocks/Collection+Mock.graphql.swift
+++ PocketKit/Sources/PocketGraphTestMocks/Collection+Mock.graphql.swift
@@ -11,9 +11,7 @@ public class Collection: MockObject {
 
   public struct MockFields {
     @Field<[CollectionAuthor]>("authors") public var authors
-    @Field<PocketGraph.Markdown>("excerpt") public var excerpt
     @Field<PocketGraph.ID>("externalId") public var externalId
-    @Field<PocketGraph.Url>("imageUrl") public var imageUrl
     @Field<PocketGraph.Markdown>("intro") public var intro
     @Field<PocketGraph.DateString>("publishedAt") public var publishedAt
     @Field<String>("slug") public var slug
@@ -25,9 +23,7 @@ public class Collection: MockObject {
 public extension Mock where O == Collection {
   convenience init(
     authors: [Mock<CollectionAuthor>]? = nil,
-    excerpt: PocketGraph.Markdown? = nil,
     externalId: PocketGraph.ID? = nil,
-    imageUrl: PocketGraph.Url? = nil,
     intro: PocketGraph.Markdown? = nil,
     publishedAt: PocketGraph.DateString? = nil,
     slug: String? = nil,
@@ -36,9 +32,7 @@ public extension Mock where O == Collection {
   ) {
     self.init()
     _set(authors, for: \.authors)
-    _set(excerpt, for: \.excerpt)
     _set(externalId, for: \.externalId)
-    _set(imageUrl, for: \.imageUrl)
     _set(intro, for: \.intro)
     _set(publishedAt, for: \.publishedAt)
     _set(slug, for: \.slug)
