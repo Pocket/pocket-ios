a2f87a6ea1b3399c23f5d039fb32fa8b5eb9713e
Gio <gruscigno@mozilla.com>
2023-07-31 16:12:26 -0500

    feat(nativeCollections): fetch authors from home

--- PocketKit/Sources/PocketGraph/Fragments/CollectionSummary.graphql.swift
+++ PocketKit/Sources/PocketGraph/Fragments/CollectionSummary.graphql.swift
@@ -8,6 +8,10 @@ public struct CollectionSummary: PocketGraph.SelectionSet, Fragment {
     fragment CollectionSummary on Collection {
       __typename
       slug
+      authors {
+        __typename
+        ...CollectionAuthorSummary
+      }
     }
     """ }
 
@@ -18,21 +22,63 @@ public struct CollectionSummary: PocketGraph.SelectionSet, Fragment {
   public static var __selections: [ApolloAPI.Selection] { [
     .field("__typename", String.self),
     .field("slug", String.self),
+    .field("authors", [Author].self),
   ] }
 
   public var slug: String { __data["slug"] }
+  public var authors: [Author] { __data["authors"] }
 
   public init(
-    slug: String
+    slug: String,
+    authors: [Author]
   ) {
     self.init(_dataDict: DataDict(
       data: [
         "__typename": PocketGraph.Objects.Collection.typename,
         "slug": slug,
+        "authors": authors._fieldData,
       ],
       fulfilledFragments: [
         ObjectIdentifier(Self.self)
       ]
     ))
+  }
+
+  /// Author
+  ///
+  /// Parent Type: `CollectionAuthor`
+  public struct Author: PocketGraph.SelectionSet {
+    public let __data: DataDict
+    public init(_dataDict: DataDict) { __data = _dataDict }
+
+    public static var __parentType: ApolloAPI.ParentType { PocketGraph.Objects.CollectionAuthor }
+    public static var __selections: [ApolloAPI.Selection] { [
+      .field("__typename", String.self),
+      .fragment(CollectionAuthorSummary.self),
+    ] }
+
+    public var name: String { __data["name"] }
+
+    public struct Fragments: FragmentContainer {
+      public let __data: DataDict
+      public init(_dataDict: DataDict) { __data = _dataDict }
+
+      public var collectionAuthorSummary: CollectionAuthorSummary { _toFragment() }
+    }
+
+    public init(
+      name: String
+    ) {
+      self.init(_dataDict: DataDict(
+        data: [
+          "__typename": PocketGraph.Objects.CollectionAuthor.typename,
+          "name": name,
+        ],
+        fulfilledFragments: [
+          ObjectIdentifier(Self.self),
+          ObjectIdentifier(CollectionAuthorSummary.self)
+        ]
+      ))
+    }
   }
 }
