f28ebe876439997a0ed8794a10519a9f23363c17
Gio <gruscigno@mozilla.com>
2023-08-06 00:30:04 -0500

    feat(nativeCollections): addressed PR changes

--- PocketKit/Sources/Sync/Collection/CollectionUrlFormatter.swift
+++ PocketKit/Sources/Sync/Collection/CollectionUrlFormatter.swift
@@ -15,7 +15,7 @@ public struct CollectionUrlFormatter {
 
     public init() {}
 
-    public func collectionUrl(_ urlString: String) -> URL? {
+    public static func collectionUrl(_ urlString: String) -> URL? {
         guard let url = URL(string: urlString), url.host == Self.host,
               url.pathComponents.count >= Self.pathComponentsCount,
               url.pathComponents[safe: Self.collectionComponentIndex] == Self.collectionComponent else {
@@ -24,7 +24,7 @@ public struct CollectionUrlFormatter {
         return url
     }
 
-    public func collectionUrlString(_ slug: String) -> String {
+    public static func collectionUrlString(_ slug: String) -> String {
         var components = URLComponents()
         components.scheme = Self.scheme
         components.host = Self.host
@@ -34,11 +34,11 @@ public struct CollectionUrlFormatter {
             .absoluteString
     }
 
-    public func isCollectionUrl(_ urlString: String) -> Bool {
+    public static func isCollectionUrl(_ urlString: String) -> Bool {
         collectionUrl(urlString) != nil
     }
 
-    public func slug(from urlString: String) -> String? {
+    public static func slug(from urlString: String) -> String? {
         guard let url = collectionUrl(urlString) else {
             return nil
         }
