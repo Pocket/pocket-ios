5a4e217192563fb5f3680dde685ec30a07b2a0f5
Gio <gruscigno@mozilla.com>
2023-08-08 00:05:53 -0500

    add migration from model 1 to model 3

--- PocketKit/Sources/Sync/PersistentContainer.swift
+++ PocketKit/Sources/Sync/PersistentContainer.swift
@@ -60,6 +60,8 @@ public class PersistentContainer: NSPersistentContainer {
         storeDescription.setOption(true as NSNumber, forKey: NSPersistentStoreRemoteChangeNotificationPostOptionKey)
         storeDescription.setOption(true as NSNumber, forKey: NSPersistentHistoryTrackingKey)
         storeDescription.setOption(true as NSNumber, forKey: NSMigratePersistentStoresAutomaticallyOption)
+        storeDescription.shouldMigrateStoreAutomatically = false
+        storeDescription.shouldInferMappingModelAutomatically = false
 
         loadPersistentStores { storeDescription, error in
             if let error = error as NSError? {
