5716105d2973865c4b88860dfbfe1627fa7f742a
Conor Masterson <cmasterson@mozilla.com>
2023-08-09 13:23:56 -0400

    fix(color) Add custom color to Palette

--- PocketKit/Sources/PocketKit/Article/Cells/ArticleComponentTextCell.swift
+++ PocketKit/Sources/PocketKit/Article/Cells/ArticleComponentTextCell.swift
@@ -3,6 +3,7 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import UIKit
+import Textile
 
 // An object that conforms to this protocol is commonly capable of responding to
 // (overridden) events that occur within a PocketTextView nested within a PocketTextCell.
@@ -60,7 +61,7 @@ class ArticleComponentTextView: UITextView {
         isScrollEnabled = false
         delegate = self
 
-        linkTextAttributes = [.foregroundColor: UIColor(named: "textfieldURL")]
+        linkTextAttributes = [.foregroundColor: ColorAsset.ui.textfieldURL as Any]
 
         interactions
             .filter { $0 is UIContextMenuInteraction }
