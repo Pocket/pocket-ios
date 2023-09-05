d76144a29676821a1f7ea6635bf1a0190e166138
Gio <gruscigno@mozilla.com>
2023-09-05 15:50:33 -0500

    feat(graph): add CorpusItem to saves

--- PocketKit/Sources/PocketGraphTestMocks/MockObject+Unions.graphql.swift
+++ PocketKit/Sources/PocketGraphTestMocks/MockObject+Unions.graphql.swift
@@ -6,6 +6,6 @@ import PocketGraph
 
 public extension MockObject {
   typealias ItemResult = Union
-  typealias MarticleComponent = Union
   typealias CorpusTarget = Union
+  typealias MarticleComponent = Union
 }
