06970058d1b6a4ec68bf2f1dfe3f397f654f2a11
David Skuza <dskuza@mozilla.com>
2023-11-08 10:10:12 -0600

    feat(listen): use offline delegation for offline tts

--- PocketKit/Sources/PocketKit/Listen/ListenViewModel.swift
+++ PocketKit/Sources/PocketKit/Listen/ListenViewModel.swift
@@ -7,10 +7,48 @@ import Sync
 import PKTListen
 import SharedPocketKit
 
-class ListenViewModel: PKTListenDataSource<PKTListDiffable> {
-    public var title: String = "Unknown"
+class ListenConfiguration: NSObject, PKTListenOfflineTTSDelegate {
+    let title: String
+    private let savedItems: [SavedItem]?
 
-    static func source(savedItems: [SavedItem]?, title: String) -> ListenViewModel {
+    init(title: String, savedItems: [SavedItem]?) {
+        self.title = title
+        self.savedItems = savedItems
+    }
+
+    func toAppConfiguration() -> PKTListenAppConfiguration {
+        let config = PKTListenAppConfiguration(source: createSource())
+        config.offlineTTSDelegate = self
+        return config
+    }
+
+    private func createSource() -> ListenSource {
+        return ListenSource.source(savedItems: savedItems)
+    }
+
+    func textUnits(for kusari: PKTKusari<PKTListenItem>) -> [PKTTextUnit] {
+        // When returning the text components for offline TTS,
+        // fetch the saved item with the given URL of the item staged for playing, ensuring it has an article
+        guard let givenURL = kusari.album?.givenURL,
+              let savedItem = savedItems?.first(where: { $0.givenURL == givenURL }),
+              let article = savedItem.item?.article
+        else {
+            return []
+        }
+
+        // For offline TTS, filter out only the text components, returning the appropriate types
+        return article.components.compactMap { c in
+            switch c {
+            // Utilize the NSAttributedString(markdown:) initializer to "strip out" the characters used for styling
+            case .text(let textComponent): return try? NSAttributedString(markdown: textComponent.content).string
+            default: return nil
+            }
+        }.map { ListenTextUnit(stringValue: $0) }
+    }
+}
+
+class ListenSource: PKTListenDataSource<PKTListDiffable> {
+    static func source(savedItems: [SavedItem]?) -> ListenSource {
         let config = PKTListenAppKusariConfiguration()
 
         let listenItems: [PKTKusari<PKTListenItem>] = savedItems?
@@ -26,12 +64,22 @@ class ListenViewModel: PKTListenDataSource<PKTListDiffable> {
             })
         }
 
-        let viewModel = ListenViewModel(context: ["index": NSNumber(value: 0)]) { source, context, complete in
+        return ListenSource(context: ["index": NSNumber(value: 0)]) { source, context, complete in
             source.hasMore = false
             Log.debug("Loaded Listen with \(listenItems.count) articles")
             complete(nil, ["index": NSNumber(value: listenItems.count)], listenItems)
         }
-        viewModel.title = title
-        return viewModel
+    }
+}
+
+private class ListenTextUnit: NSObject, PKTTextUnit {
+    var type: PKTTagMatchType = .text
+
+    var stringValue: String?
+
+    var tagValue: String?
+
+    init(stringValue: String) {
+        self.stringValue = stringValue
     }
 }
