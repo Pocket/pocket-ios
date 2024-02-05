6def7afbc5825b99b9e09f51ade036a4f7496741
Daniel Brooks <bass_rock@me.com>
2024-02-05 08:53:53 -0800

    chore(deps): update sentry to 8.20

--- PocketKit/Package.swift
+++ PocketKit/Package.swift
@@ -28,7 +28,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apollographql/apollo-ios.git", exact: "1.9.0"),
         .package(url: "https://github.com/onevcat/Kingfisher.git", exact: "7.10.2"),
-        .package(url: "https://github.com/getsentry/sentry-cocoa.git", exact: "8.19.0"),
+        .package(url: "https://github.com/getsentry/sentry-cocoa.git", exact: "8.20.0"),
         .package(url: "https://github.com/snowplow/snowplow-objc-tracker", exact: "6.0.0"),
         .package(url: "https://github.com/ccgus/fmdb", from: "2.7.6"),
         .package(url: "https://github.com/airbnb/lottie-ios.git", exact: "4.4.0"),
