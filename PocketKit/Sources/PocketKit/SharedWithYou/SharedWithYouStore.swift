115625d317b07cde1bffd9f451693a6a4a24981f
Gio <gruscigno@mozilla.com>
2024-03-05 17:49:24 -0600

    feat(Shared With You): inject appSession in SharedWithYouStore to start/stop the service when user is loggedIn/out

--- PocketKit/Sources/PocketKit/SharedWithYou/SharedWithYouStore.swift
+++ PocketKit/Sources/PocketKit/SharedWithYou/SharedWithYouStore.swift
@@ -2,7 +2,9 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import Combine
 import Foundation
+import SharedPocketKit
 import SharedWithYou
 import Sync
 
@@ -10,18 +12,25 @@ import Sync
 final class SharedWithYouStore: NSObject {
     private let highlightCenter: SWHighlightCenter
     private let source: Source
+    private let appSession: AppSession
+
+    private var subscriptions = Set<AnyCancellable>()
 
     private(set) var highlights: [SWHighlight]
 
-    init(highlightCenter: SWHighlightCenter? = nil, source: Source) {
+    init(highlightCenter: SWHighlightCenter? = nil, source: Source, appSession: AppSession) {
         self.highlightCenter = highlightCenter ?? SWHighlightCenter()
         self.highlights = self.highlightCenter.highlights
         self.source = source
+        self.appSession = appSession
         super.init()
-        self.highlightCenter.delegate = self
+        if appSession.currentSession != nil {
+            start()
+        }
     }
 }
 
+// MARK: Shared With You delegate
 extension SharedWithYouStore: SWHighlightCenterDelegate {
     /// Emits changes in the shared with you list associated with the app
     func highlightCenterHighlightsDidChange(_ highlightCenter: SWHighlightCenter) {
@@ -32,5 +41,32 @@ extension SharedWithYouStore: SWHighlightCenterDelegate {
             // via the associated RichFetchedResultController
             source.updateSharedWithYouItems(with: highlightCenter.highlights.map { $0.url.absoluteString })
         }
+    }
+}
+
+// MARK: private helpers
+private extension SharedWithYouStore {
+    func start() {
+        self.highlightCenter.delegate = self
+    }
+
+    func stop() {
+        self.highlightCenter.delegate = nil
+    }
+
+    func listenForUserSession() {
+        // Register for login notifications
+        NotificationCenter.default.publisher(
+            for: .userLoggedIn
+        ).sink { [weak self] _ in
+            self?.start()
+        }.store(in: &subscriptions)
+
+        // Register for logout notifications
+        NotificationCenter.default.publisher(
+            for: .userLoggedOut
+        ).sink { [weak self] _ in
+            self?.stop()
+        }.store(in: &subscriptions)
     }
 }
