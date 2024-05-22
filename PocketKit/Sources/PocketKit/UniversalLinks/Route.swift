f6744f3bc6d0fcfae880b674ec791d16f89d646f
Gio <gruscigno@mozilla.com>
2024-05-22 14:14:28 -0500

    feat(alternate icons): add a new route to select the icon switcher from a braze action
    feat(alternate icons): replace temporary pride icon with monochrome icon

--- PocketKit/Sources/PocketKit/UniversalLinks/Route.swift
+++ PocketKit/Sources/PocketKit/UniversalLinks/Route.swift
@@ -229,3 +229,26 @@ struct PocketReadRoute: Route {
         return normalizedComponents.url?.absoluteString
     }
 }
+
+@MainActor
+struct BrazeIconSwitcherRoute: Route {
+    let host: String? = "pocket.co"
+    let scheme = "https"
+    let path = "/braze/iconSwitcher"
+    let source: ReadableSource = .external
+    let action: (URL, ReadableSource) -> Void
+
+    init(action: @escaping (URL, ReadableSource) -> Void) {
+        self.action = action
+    }
+
+    nonisolated func matchedUrlString(from url: URL) -> String? {
+        guard let components = URLComponents(url: url, resolvingAgainstBaseURL: true),
+              components.host == host,
+              components.scheme == scheme,
+              components.path.contains(path) else {
+            return nil
+        }
+        return url.absoluteString
+    }
+}
