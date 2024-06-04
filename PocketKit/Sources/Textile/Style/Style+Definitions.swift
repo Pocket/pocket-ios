6fcd88e5e8b6ba3311c04730ccb0471c9dad6b10
Gio <gruscigno@mozilla.com>
2024-06-04 15:50:16 -0500

    feat(concurrency): conform text styles to sendable

--- PocketKit/Sources/Textile/Style/Style+Definitions.swift
+++ PocketKit/Sources/Textile/Style/Style+Definitions.swift
@@ -111,7 +111,7 @@ public extension Style {
         }
     }
 
-    struct Body {
+    struct Body: Sendable {
         public let sansSerif = Style(family: .graphik, size: .body, weight: .regular)
         public let serif = Style(family: .blanco, size: .body, weight: .regular)
         public let monospace = Style(family: .monospace, size: .monospace, weight: .regular)
