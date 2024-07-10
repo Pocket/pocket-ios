f7c6d6777e9883605b38103b47293d418c28e2e1
Gio <gruscigno@mozilla.com>
2024-07-10 09:12:13 -0500

    feat(signed out): add SessionType to Session

--- PocketKit/Sources/SharedPocketKit/Session/Session.swift
+++ PocketKit/Sources/SharedPocketKit/Session/Session.swift
@@ -4,14 +4,33 @@
 
 import Foundation
 
-public struct Session: Codable, Equatable {
+public struct Session: Codable, Equatable, Sendable {
     public let guid: String
     public let accessToken: String
     public let userIdentifier: String
+    public let sessionType: SessionType
 
-    public init(guid: String, accessToken: String, userIdentifier: String) {
+    public init(guid: String, accessToken: String, userIdentifier: String, sessionType: SessionType = .authenticated) {
         self.guid = guid
         self.accessToken = accessToken
         self.userIdentifier = userIdentifier
+        self.sessionType = sessionType
+    }
+}
+
+extension Session {
+    public enum SessionType: String, Codable, Sendable {
+        case authenticated
+        case anonymous
+    }
+
+    /// True if the session is anonymous
+    public var isAnonymous: Bool {
+        sessionType == .anonymous
+    }
+    /// Instantiates an anonymous session, used for the signed out experience
+    /// - Returns: a session with anonymous identifiers & tokens
+    public static func anonymous() -> Session {
+        Session(guid: "", accessToken: "", userIdentifier: "", sessionType: .anonymous)
     }
 }
