4fe935e07c23f095cc46b7190aef677172c4b85a
Gio <gruscigno@mozilla.com>
2024-07-12 14:44:38 -0500

    fix(collections): update CollectionUrlFormatter, add support for localized collections (e.g. /de/collections/[slug])

--- PocketKit/Sources/Sync/Collection/CollectionUrlFormatter.swift
+++ PocketKit/Sources/Sync/Collection/CollectionUrlFormatter.swift
@@ -5,23 +5,36 @@
 import Foundation
 
 /// A formatter for collection URLs
+/// valid formats are
+/// https://getpocket.com/collections/[slug] (unlocalized)
+/// https://getpocket.com/[locale]/collections/[slug] (localized)
 public struct CollectionUrlFormatter {
     static let scheme = "https"
     static let host = "getpocket.com"
     static let collectionComponent = "collections"
     static let collectionComponentIndex = 1
+    static let localizedCollectionComponentIndex = 2
     static let slugComponentIndex = 2
-    static let pathComponentsCount = 3
+    static let localizedSlugComponentIndex = 3
+    static let minPathComponentsCount = 3
 
     public init() {}
 
-    public static func collectionUrl(_ urlString: String) -> URL? {
+    private static func collectionUrl(_ urlString: String, collectionIndex: Int) -> URL? {
         guard let url = URL(string: urlString), url.host == Self.host,
-              url.pathComponents.count >= Self.pathComponentsCount,
-              url.pathComponents[safe: Self.collectionComponentIndex] == Self.collectionComponent else {
+              url.pathComponents.count >= Self.minPathComponentsCount,
+              url.pathComponents[safe: collectionIndex] == Self.collectionComponent else {
             return nil
         }
         return url
+    }
+
+    private static func unlocalizedCollectionUrl(_ urlString: String) -> URL? {
+        collectionUrl(urlString, collectionIndex: Self.collectionComponentIndex)
+    }
+
+    private static func localizedCollectionUrl(_ urlString: String) -> URL? {
+        collectionUrl(urlString, collectionIndex: Self.localizedCollectionComponentIndex)
     }
 
     public static func collectionUrlString(_ slug: String) -> String {
@@ -35,13 +48,19 @@ public struct CollectionUrlFormatter {
     }
 
     public static func isCollectionUrl(_ urlString: String) -> Bool {
-        collectionUrl(urlString) != nil
+        unlocalizedCollectionUrl(urlString) ?? localizedCollectionUrl(urlString) != nil
     }
 
+    /// Extracts the slug from a collection url, if the passed string maps to a valid collection url
+    /// - Parameter urlString: the url string
+    /// - Returns: the slug or nil
     public static func slug(from urlString: String) -> String? {
-        guard let url = collectionUrl(urlString) else {
-            return nil
+        if let url = unlocalizedCollectionUrl(urlString) {
+            return url.pathComponents[safe: Self.slugComponentIndex]
         }
-        return url.pathComponents[safe: Self.slugComponentIndex]
+        if let url = localizedCollectionUrl(urlString) {
+            return url.pathComponents[safe: Self.localizedSlugComponentIndex]
+        }
+        return nil
     }
 }
