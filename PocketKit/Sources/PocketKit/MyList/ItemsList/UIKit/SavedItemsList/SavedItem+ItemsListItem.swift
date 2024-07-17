c810fa72417e5b25a0017ace16295507da149ada
Gio <gruscigno@mozilla.com>
2024-07-17 01:28:29 -0500

    remove unnecessary change from SavedItem+ItemListItem

--- PocketKit/Sources/PocketKit/MyList/ItemsList/UIKit/SavedItemsList/SavedItem+ItemsListItem.swift
+++ PocketKit/Sources/PocketKit/MyList/ItemsList/UIKit/SavedItemsList/SavedItem+ItemsListItem.swift
@@ -25,7 +25,7 @@ extension SavedItem: ItemsListItem {
 
     var displayTitle: String {
         let itemTitle = item?.title?.isEmpty == false ? item?.title : nil
-        return itemTitle ?? item?.collection?.title ?? item?.bestURL ?? url
+        return itemTitle ?? item?.bestURL ?? url
     }
 
     var displayDomain: String? {
