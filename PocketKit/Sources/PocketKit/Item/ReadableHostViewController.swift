3db08ba0641c0e764165099ff61d62b011bb046f
Gio <gruscigno@mozilla.com>
2024-08-07 13:33:16 -0500

    feat(signed out): update ReadableViewModel and implementations, remove analyticsSource param since it can be directly injected in the call, because the only option here is syndicated article

--- PocketKit/Sources/PocketKit/Item/ReadableHostViewController.swift
+++ PocketKit/Sources/PocketKit/Item/ReadableHostViewController.swift
@@ -195,7 +195,7 @@ class ReadableHostViewController: UIViewController {
 
     @objc
     private func save() {
-        readableViewModel.save(.syndicatedArticle) { [weak self] success in
+        readableViewModel.save { [weak self] success in
             if success,
                let items = self?.navigationItem.rightBarButtonItems,
                let saveButton = self?.saveButton,
