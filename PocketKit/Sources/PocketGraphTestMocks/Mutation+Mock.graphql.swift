14656f605b65cceac201f91dcf34b3a0320ba404
Gio <gruscigno@mozilla.com>
2024-08-15 16:11:38 -0500

    feat(tags): add replace tags mutation

--- PocketKit/Sources/PocketGraphTestMocks/Mutation+Mock.graphql.swift
+++ PocketKit/Sources/PocketGraphTestMocks/Mutation+Mock.graphql.swift
@@ -15,6 +15,8 @@ public class Mutation: MockObject {
     @Field<PocketGraph.ID>("deleteSavedItemHighlight") public var deleteSavedItemHighlight
     @Field<PocketGraph.ID>("deleteTag") public var deleteTag
     @Field<PocketGraph.ID>("deleteUser") public var deleteUser
+    @available(*, deprecated, message: "use saveBatchUpdateTags")
+    @Field<[SavedItem]>("replaceSavedItemTags") public var replaceSavedItemTags
     @Field<SavedItem>("savedItemArchive") public var savedItemArchive
     @Field<PocketGraph.Url>("savedItemDelete") public var savedItemDelete
     @Field<SavedItem>("savedItemFavorite") public var savedItemFavorite
@@ -34,6 +36,7 @@ public extension Mock where O == Mutation {
     deleteSavedItemHighlight: PocketGraph.ID? = nil,
     deleteTag: PocketGraph.ID? = nil,
     deleteUser: PocketGraph.ID? = nil,
+    replaceSavedItemTags: [Mock<SavedItem>]? = nil,
     savedItemArchive: Mock<SavedItem>? = nil,
     savedItemDelete: PocketGraph.Url? = nil,
     savedItemFavorite: Mock<SavedItem>? = nil,
@@ -49,6 +52,7 @@ public extension Mock where O == Mutation {
     _setScalar(deleteSavedItemHighlight, for: \.deleteSavedItemHighlight)
     _setScalar(deleteTag, for: \.deleteTag)
     _setScalar(deleteUser, for: \.deleteUser)
+    _setList(replaceSavedItemTags, for: \.replaceSavedItemTags)
     _setEntity(savedItemArchive, for: \.savedItemArchive)
     _setScalar(savedItemDelete, for: \.savedItemDelete)
     _setEntity(savedItemFavorite, for: \.savedItemFavorite)
