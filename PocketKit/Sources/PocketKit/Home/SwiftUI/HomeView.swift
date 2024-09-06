a6545059cac9d2d97569ca99da6cd88e6d8ae08c
Gio <gruscigno@mozilla.com>
2024-09-06 18:32:51 -0500

    feat(swiftui home): adjust list margins

--- PocketKit/Sources/PocketKit/Home/SwiftUI/HomeView.swift
+++ PocketKit/Sources/PocketKit/Home/SwiftUI/HomeView.swift
@@ -13,6 +13,7 @@ struct HomeView: View {
         List(recommendations) { recommendation in
             if let item = recommendation.item {
                 HomeHeroView(model: HomeItemCellViewModel2(item: item, imageURL: item.topImageURL))
+                    .listRowInsets(EdgeInsets(top: 8, leading: 12, bottom: 8, trailing: 12))
             }
         }
         .listStyle(.plain)
