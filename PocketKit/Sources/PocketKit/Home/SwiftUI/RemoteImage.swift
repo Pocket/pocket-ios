a2899789f700c49735e7d5a5449f9fda2cd16bd7
Gio <gruscigno@mozilla.com>
2024-09-10 11:17:53 -0500

    feat(swiftui home): add RemoteImage, a KingFisher loaded remote image with resizing and cropping processors, and an optional grey background if the image is not found

---
+++ PocketKit/Sources/PocketKit/Home/SwiftUI/RemoteImage.swift
@@ -0,0 +1,43 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import Kingfisher
+import SwiftUI
+import Textile
+
+/// A resizable remote image backed by `KingFisher`
+/// if `url` is nil and  `usePlaceholder` is true it will return a placeholder view
+/// if `url` is nil and `usePlaceholder` is false, it will return an empty view
+struct RemoteImage: View {
+    let url: URL?
+    let imageSize: CGSize
+    let usePlaceholder: Bool = true
+
+    var body: some View {
+        if let url {
+            KFImage(url)
+                .placeholder {
+                    ProgressView()
+                        .progressViewStyle(CircularProgressViewStyle(tint: Color(.ui.grey4)))
+                }
+                .setProcessor(
+                    ResizingImageProcessor(
+                        referenceSize: imageSize,
+                        mode: .aspectFill
+                    )
+                    .append(
+                        another: CroppingImageProcessor(
+                            size: imageSize
+                        )
+                    )
+                )
+                .callbackQueue(.dispatch(.global(qos: .userInteractive)))
+                .backgroundDecode()
+                .scaleFactor(UIScreen.main.scale)
+                .resizable()
+        } else if usePlaceholder {
+            Color(.ui.grey6)
+        }
+    }
+}
