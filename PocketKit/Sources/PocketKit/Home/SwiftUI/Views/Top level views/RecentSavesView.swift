54a41ee31968cabee1fd7249b69b2bef81099a34
Gio <gruscigno@mozilla.com>
2024-09-27 11:57:47 -0500

    feat(swiftui home): conditionally update recent saves and shared with you only if the list actually change, to improve performance

--- PocketKit/Sources/PocketKit/Home/SwiftUI/Views/Top level views/RecentSavesView.swift
+++ PocketKit/Sources/PocketKit/Home/SwiftUI/Views/Top level views/RecentSavesView.swift
@@ -10,27 +10,52 @@ import Textile
 
 struct RecentSavesView: View {
     @Query private var savedItems: [SavedItem]
+
     @EnvironmentObject private var mainViewModel: MainViewModel
+
+    @State private var cards: [HomeCard] = []
 
     init() {
         let predicate = #Predicate<SavedItem> { $0.isArchived == false && $0.deletedAt == nil }
         let sortDescriptor = SortDescriptor<SavedItem>(\.createdAt, order: .reverse)
         var fetchDescriptor = FetchDescriptor<SavedItem>(predicate: predicate, sortBy: [sortDescriptor])
         fetchDescriptor.fetchLimit = 5
-        _savedItems = Query(fetchDescriptor, animation: .easeIn)
+        _savedItems = Query(fetchDescriptor)
     }
 
     var body: some View {
-        if !carouselCards.isEmpty {
-            LazyVStack(alignment: .leading, spacing: 0) {
-                makeHeader()
-                CarouselView(cards: carouselCards, useGrid: false)
+        ZStack {
+            makeBody()
+        }
+        // TODO: SWIFTUI -  the animation included with @Query does not seem to behave as we want, so we do it here
+        .onChange(of: savedItems, initial: true) {
+            // this prevents unwanted view refreshes if the query updates (because Core Data receives updates)
+            // but the recent saves do not actually change
+            if proposedCards != cards {
+                cards = proposedCards
             }
         }
+        // TODO: SWIFTUI - this animation works well when removing on top of the list, not so well otherwise
+        // investigate better animation options
+        .animation(.smooth, value: cards)
     }
 }
 
 private extension RecentSavesView {
+    @ViewBuilder
+    func makeBody() -> some View {
+        if !cards.isEmpty {
+            LazyVStack(alignment: .leading, spacing: 0) {
+                makeHeader()
+                CarouselView(cards: cards, useGrid: false)
+                    .padding(.bottom, 32)
+            }
+        } else {
+            Spacer()
+                .frame(height: .zero)
+        }
+    }
+
     func makeHeader() -> some View {
         SectionHeader(title: Localization.recentSaves) {
             mainViewModel.selectedSection = .saves
@@ -39,7 +64,7 @@ private extension RecentSavesView {
         .padding(.trailing, 16)
     }
 
-    var carouselCards: [HomeCard] {
+    var proposedCards: [HomeCard] {
         savedItems.compactMap {
             if let item = $0.item {
                 return HomeCard(
