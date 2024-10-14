58c67f07b089dd92949c533ac56004c62a371d28
Gio <34376330+Gio2018@users.noreply.github.com>
2024-10-14 16:17:45 -0500

    Update Saves analytics to the new analytics events (#1090)
    * feat(saves): update analytics to the new model
    
    * feat(saves): add saves/archive component detail to analytics events

--- PocketKit/Sources/Analytics/AppEvents/Saves.swift
+++ PocketKit/Sources/Analytics/AppEvents/Saves.swift
@@ -17,13 +17,27 @@ public extension Events.Saves {
     static func contentOpen(
         destination: ContentOpen.Destination,
         trigger: ContentOpen.Trigger = .click,
-        url: String
+        url: String,
+        listType: String?
     ) -> ContentOpen {
         return ContentOpen(
             destination: destination,
             trigger: trigger,
             contentEntity: ContentEntity(url: url),
-            uiEntity: UiEntity(.card, identifier: "saves.card.open")
+            uiEntity: UiEntity(.card, identifier: "saves.card.open", componentDetail: listType)
+        )
+    }
+
+    static func cardViewed(_ index: Int?, listType: String?) -> Impression {
+        return Impression(
+            component: .card,
+            requirement: .viewable,
+            uiEntity: UiEntity(
+                .card,
+                identifier: "saves.card.impression",
+                componentDetail: listType,
+                index: index
+            )
         )
     }
 
@@ -43,5 +57,92 @@ public extension Events.Saves {
         let identifier = saveSucceeded ? "saves.addItem.success" : "saves.addItem.fail"
 
         return Engagement(uiEntity: UiEntity(.button, identifier: identifier))
+    }
+
+    static func archiveItem(_ index: Int?, listType: String?) -> Engagement {
+        Engagement(
+            uiEntity: UiEntity(
+                .card,
+                identifier: "saves.archive",
+                componentDetail: listType,
+                index: index
+            )
+        )
+    }
+
+    static func favoriteItem(_ index: Int?, listType: String?) -> Engagement {
+        Engagement(
+            uiEntity: UiEntity(
+                .card,
+                identifier: "saves.favorite",
+                componentDetail: listType,
+                index: index
+            )
+        )
+    }
+
+    static func unFavoriteItem(_ index: Int?, listType: String?) -> Engagement {
+        Engagement(
+            uiEntity: UiEntity(
+                .card,
+                identifier: "saves.un-favorite",
+                componentDetail: listType,
+                index: index
+            )
+        )
+    }
+
+    static func unArchiveItem(_ index: Int?, listType: String?) -> Engagement {
+        Engagement(
+            uiEntity: UiEntity(
+                .card,
+                identifier: "saves.unarchive",
+                index: index
+            )
+        )
+    }
+
+    static func deleteItem(_ index: Int?, listType: String?) -> Engagement {
+        Engagement(
+            uiEntity: UiEntity(
+                .card,
+                identifier: "saves.delete",
+                componentDetail: listType,
+                index: index
+            )
+        )
+    }
+
+    static func shareItem(_ index: Int?, listType: String?) -> Engagement {
+        Engagement(
+            uiEntity: UiEntity(
+                .card,
+                identifier: "saves.share",
+                componentDetail: listType,
+                index: index
+            )
+        )
+    }
+
+    static func overflow(_ index: Int?, listType: String?) -> Engagement {
+        Engagement(
+            uiEntity: UiEntity(
+                .card,
+                identifier: "saves.overflow",
+                componentDetail: listType,
+                index: index
+            )
+        )
+    }
+
+    static func overflowEditTags(_ index: Int?, listType: String?) -> Engagement {
+        Engagement(
+            uiEntity: UiEntity(
+                .card,
+                identifier: "saves.overflow.edittags",
+                componentDetail: listType,
+                index: index
+            )
+        )
     }
 }
