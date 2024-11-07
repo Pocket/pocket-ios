99a442cf049a8c1c55cda47b95f980fc53f35882
Gio <gruscigno@mozilla.com>
2024-11-07 12:54:34 -0600

    rfeat(swiftui home): add NetworkMonitor, and observable type that publishes network status changes.

--- PocketKit/Sources/PocketKit/Home/SwiftUI/Utilities/NetworkMonitor.swift
+++ PocketKit/Sources/PocketKit/Home/SwiftUI/Utilities/NetworkMonitor.swift
@@ -3,3 +3,27 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import Foundation
+import Sync
+import Network
+
+final class NetworkMonitor: ObservableObject {
+    private let monitor: NetworkPathMonitor
+
+    @Published private(set) var status: NWPath.Status = .satisfied
+
+    init() {
+        self.monitor = NWPathMonitor()
+        monitor.updateHandler = { [weak self] path in
+            self?.status = path.status
+        }
+
+        func start(queue: DispatchQueue? = nil) {
+            let queue = queue ?? DispatchQueue.global(qos: .utility)
+            monitor.start(queue: queue)
+        }
+
+        func cancel() {
+            monitor.cancel()
+        }
+    }
+}
