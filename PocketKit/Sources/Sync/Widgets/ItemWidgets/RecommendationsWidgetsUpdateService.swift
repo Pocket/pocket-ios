d061f98cd0d5f418b88806791c8ad0c602d4b834
Gio <gruscigno@mozilla.com>
2025-01-31 16:53:50 -0600

    fix(widgets): update TopicRecommendationsWidget, use a different widget kind from the static one

--- PocketKit/Sources/Sync/Widgets/ItemWidgets/RecommendationsWidgetsUpdateService.swift
+++ PocketKit/Sources/Sync/Widgets/ItemWidgets/RecommendationsWidgetsUpdateService.swift
@@ -52,6 +52,6 @@ public struct RecommendationsWidgetUpdateService {
 
     /// Reloads the widget
     private func reloadWidget() {
-        WidgetCenter.shared.reloadTimelines(ofKind: WidgetKind.recommendations)
+        WidgetCenter.shared.reloadTimelines(ofKind: WidgetKind.topicRecommendations)
     }
 }
